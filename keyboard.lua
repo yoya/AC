2198a24776c7fd053228a0486fb879205829c801
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-05 18:02:26 +0900

    pushKeys 実装

---
+++ keyboard.lua
@@ -0,0 +1,20 @@
+
+---
+--- Keyboard
+--- キーボード操作の関数
+
+local M = {}
+
+function M.pushKeys(keys)
+    local command = ""
+    local wait = 0
+    for i, k in ipairs(keys) do
+        command = command.."setkey "..k.." down; wait 0.1; setkey "..k.." up; wait 0.1; "
+        wait = wait + 0.1 + 0.1
+    end
+    windower.send_command(command)
+    coroutine.sleep(wait)
+end
+
+return M
+
