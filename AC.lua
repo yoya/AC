1efdc2078d547afbc8b5b3ed395bd1bd94fc04cf
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-05 18:03:03 +0900

    ワークス応援の自動化。（実装途中）

--- AC.lua
+++ AC.lua
@@ -104,6 +104,7 @@ local getSendCommandProbTable = aprob.getSendCommandProbTable
 local autopos = require 'autopos'
 local autozone = require 'autozone'
 local autoincoming = require 'autoincoming'
+local keyboard = require 'keyboard'
 
 local JunkItems = item_junkitem.JunkItems
 local _JunkItems = item_junkitem._JunkItems
@@ -391,8 +392,8 @@ local figtingFunction = function()
     local tp100Jobs = S{-"RNG", "BLM", "SMN", "SCH"}
     --- WAR はスチサイ用。DNC はダンス用？
     local tpJobs = S{"WAR"} --- "DNC"
-    local tpMin = 1500
-    local tpMax = 2000
+    local tpMin = 1200
+    local tpMax = 1500
     if tp100Jobs:contains(player.main_job) then
         tpMin = 1050
         tpMax = 1150
@@ -658,6 +659,115 @@ local idleFunctionWestAdoulin = function()
     end
 end
 
+local left_move = function(t)
+    windower.send_command('setkey a down')
+    coroutine.sleep(t)
+    windower.send_command('setkey a up')
+end
+local right_move = function(t)
+    windower.send_command('setkey d down')
+    coroutine.sleep(t)
+    windower.send_command('setkey d up')
+end
+
+local target_lockon = function(b)
+    local player = windower.ffxi.get_player()
+    local locked = player.target_locked
+    if b ~= locked then
+        windower.send_command('input /lockon')
+    end
+end
+
+local N  = 0
+local NE = math.pi * (1/4)
+local E  = math.pi * (2/4)
+local SE = math.pi * (3/4)
+local S  = math.pi
+local SW = math.pi * (5/4)
+local W  = math.pi * (6/4)
+local NW = math.pi * (7/4)
+
+-- 0:喜ぶ 1:泣く 2:驚く 3:悔しむ
+-- 4:励ます 5:慌てる 6:照れる 7:気合
+
+local stationWorkerBoostTable = {
+    [265] = {
+        [0]  = {SE,S, 4}, [3]  = {S,SW, 5},
+        [6]  = {SW,W, 6}, [9]  = {W,NW, 7},
+        [12] = {NW,N, 0}, [15] = {N,NW, 1},
+        [18] = {NE,E, 2}, [21] = {E,SE, 3},
+    },
+}
+
+local getStationWorkerBoostInfo = function()
+    local zone = windower.ffxi.get_info().zone
+    local info = windower.ffxi.get_info()
+    local time = info.time
+    local timeTable = stationWorkerBoostTable[zone]
+    for t, i in pairs(timeTable) do
+        if t*60 <= time and time < (t+3)*60 then
+            return i
+        end
+    end
+    return nil
+end 
+
+-- 範囲は -math.pi < theta < math.pi
+local normalangle = function(a)
+    if a <= math.pi or math.pi <= a then
+        a = a % (2*math.pi)
+    end
+    if a < math.pi then
+        return a
+    end
+    return a - (2*math.pi)
+end
+
+local midangle = function(a, b)
+    a = normalangle(a)
+    b = normalangle(b)
+    if math.abs(a-b) < math.pi then
+        return normalangle((a + b) / 2)
+    end
+    a = a % (2*math.pi)
+    b = b % (2*math.pi)
+    return normalangle(a - b)
+end
+
+local stationWorkerFunction = function(zone, mob)
+    print(zone, mob.x, mob.y)
+    target_lockon(true)
+    local info = getStationWorkerBoostInfo()
+    local dist = 0.5
+    -- N = 0, E = 3.14*0.5, S = 3.14, W = 3.14*1.5 
+    local theta = midangle(info[1], info[2])
+    printChat("theta: "..theta.." select:"..info[3])
+    local done = false
+    -- 向きをあわせる
+    while done == false and auto do
+        local me_pos = {}
+        getMobPosition(me_pos, "me")
+        local dx = me_pos.x - mob.x
+        local dy = me_pos.y - mob.y
+        local t = math.atan2(dx, dy)
+        local dt = (theta % (2*math.pi)) - (t % (2*math.pi))
+        left_move(0.05)
+--        printChat("theta:"..theta.." t:"..t.." dt:"..dt)
+        if math.abs(dt) < 2*math.pi/32 then
+            done = true
+        end
+    end
+    -- 応援方法を選択する
+    keyboard.pushKeys({"enter"})
+    coroutine.sleep(2)
+    for i = 0, info[2] do
+        pushKeys({"down"})
+        printChat("Down")
+        coroutine.sleep(0.5)
+    end
+    auto = false
+end
+
 local idleFunction = function()
 --  87print("idleFunction")
     local ret
@@ -697,6 +807,10 @@ local idleFunction = function()
         if mob and mob.name == "Malgrom" then
             idleFunctionSellJunkItems()
         end
+    end
+    -- ワークス応援
+    if mob.name == "Station Worker" then
+        stationWorkerFunction(zone, mob)
     end
     if mob.name == "Ergon Locus" then
         pushKeys({"numpad5"})
