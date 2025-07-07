dc01a0d1bec0186d4f6b50eea3b99a9030e1c873
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-08 00:13:53 +0900

    南サンドリア[S]のファイル名を略称にする。

--- zone.lua
+++ zone.lua
@@ -14,29 +14,29 @@ local zone_50_WhiteGate = require('zone/50_WhiteGate')
 local zone_51_Wajaom = require('zone/51_Wajaom')
 local zone_61_Zhayolm = require('zone/61_Zhayolm')
 local zone_68_Aydeewa = require('zone/68_Aydeewa')
-local zone_80_Southern_SandOria_S = require('zone/80_Southern_SandOria_S')
+local zone_80_SSandOria_S = require('zone/80_SSandOria_S')
 local zone_82_Jugner_S = require('zone/82_Jugner_S')
 local zone_83_Vunkerl_S = require('zone/83_Vunkerl_S')
 local zone_89_Grauberg_S = require('zone/89_Grauberg_S')
 local zone_273_WohGates = require('zone/273_WohGates')
 
 M.zoneTable = {
-    [2] = zone_2_CpntLanding,   -- ギルド桟橋
-    [6] = zone_6_BearclawPn,    -- 熊爪嶽
-    [7] = zone_7_Attohwa,       -- アットワ地溝
-    [15] = zone_15_AbysKonscht  -- アビセア-コンシュタット
-    [25] = zone_25_Misareaux,   -- ミザレオ海岸
-    [26] = zone_26_Tavnazia,    -- タブナジア地下壕
+    [2] = zone_2_CpntLanding,    -- ギルド桟橋
+    [6] = zone_6_BearclawPn,     -- 熊爪嶽
+    [7] = zone_7_Attohwa,        -- アットワ地溝
+    [15] = zone_15_AbysKonscht   -- アビセア-コンシュタット
+    [25] = zone_25_Misareaux,    -- ミザレオ海岸
+    [26] = zone_26_Tavnazia,     -- タブナジア地下壕
     [45] = zone_45_AbysTahrongi, -- アビセア-タロンギ
-    [50] = zone_50_WhiteGate,   -- アトルガン白門
-    [51] = zone_51_Wajaom,      -- ワジャーム樹林
-    [61] = zone_61_Zhayolm,     -- ゼオルム火山
-    [68] = zone_68_Aydeewa,     -- エジワ蘿洞
-    [80] = zone_80_Southern_SandOria_S, -- 南サンドリア〔Ｓ〕
-    [82] = zone_82_Jugner_S,    -- ジャグナー森林〔Ｓ〕
-    [83] = zone_83_Vunkerl_S,   -- 過去ブンカール
-    [89] = zone_89_Grauberg_S,  -- 過去グロウベルグ
-    [273] = zone_273_WohGates,  -- ウォーの門
+    [50] = zone_50_WhiteGate,    -- アトルガン白門
+    [51] = zone_51_Wajaom,       -- ワジャーム樹林
+    [61] = zone_61_Zhayolm,      -- ゼオルム火山
+    [68] = zone_68_Aydeewa,      -- エジワ蘿洞
+    [80] = zone_80_SSandOria_S,  -- 南サンドリア〔Ｓ〕
+    [82] = zone_82_Jugner_S,     -- ジャグナー森林〔Ｓ〕
+    [83] = zone_83_Vunkerl_S,    -- 過去ブンカール
+    [89] = zone_89_Grauberg_S,   -- 過去グロウベルグ
+    [273] = zone_273_WohGates,   -- ウォーの門
 }
 
 return M
