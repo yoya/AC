99295be2e27646ac014c004ec366c4409034113c
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-10 02:04:29 +0900

    probTable の BLM 分を job/BLM.lua に移動

---
+++ job.lua
@@ -0,0 +1,11 @@
+--- Job 毎の情報
+
+local M = {}
+
+local BLM = require('job/BLM')
+
+M.jobTable = {
+    BLM = BLM, -- 黒魔道士
+}
+
+return M
--- prob.lua
+++ prob.lua
@@ -6,6 +6,7 @@ local utils = require 'utils'
 local merge_lists = utils.merge_lists
 local merge_tables = utils.merge_tables
 local command = require 'command'
+local acjob = require 'job'
 local M = {}
 
 --- job = { probPermil(1/1000), recast, command, wait }
@@ -84,39 +85,7 @@ local sendCommandProbTable = {
 --        { 100, 60, 'input /ma ホーリーII <t>', 3 },
         { 100, 600, 'input /ja デヴォーション <p2>', 0 },
     },
-    BLM = {
---     { 200, 10, 'input /ma ブリザド <t>', 2 },
-        { 200, 10, 'input /ma ブリザドII <t>', 2 },
-        { 300, 5, 'input /ma ブリザドIII <t>', 3 },
-        { 200, 10, 'input /ma ブリザドIV <t>', 4 },
---      { 100, 10, 'input /ma ブリザドV <t>', 5 },
---      { 100,10, 'input /ma ファイア <t>', 2 },
-        { 200,10, 'input /ma ファイアII <t>', 2 },
-        { 300, 10, 'input /ma ファイアIII <t>', 3 },
-        { 200, 10, 'input /ma ファイアIV <t>', 4 },
---        { 300, 10, 'input /ma ファイアV <t>', 5 },
---
---          { 100,10, 'input /ma サンダー <t>', 2 },
-        { 200, 5, 'input /ma サンダーII <t>', 2 },
-        { 300, 5, 'input /ma サンダーIII <t>', 3 },
-        { 200, 10, 'input /ma サンダーIV <t>', 4 },
---        { 200, 10, 'input /ma サンダーV <t>', 5 },
---        { 200, 10, 'input /ma サンダーVI <t>', 5 },
-        { 500, 90, 'input /ma バーン <t>', 5 },
-        { 500, 90, 'input /ma チョーク <t>', 5 },
--- スキル上げ
---        { 500, 10, 'input /ma ストーン <t>', 2 },
---        { 200, 10, 'input /ma ファイア <t>', 2 },
---        { 500, 10, 'input /ma ブリザド <t>', 2 },
---        { 500, 10, 'input /ma サンダー <t>', 2 },
---        { 300,10, 'input /ma バイオ <t>', 2 },
-        { 300,40/2, 'input /ma ドレイン <t>', 2 },
---        { 200,10, 'input /ma スタン <t>', 2 },
-         { 200,10, 'input /ma アスピル <t>', 2 },
-          { 200,10, 'input /ma アスピルII <t>', 2 },
---        { 200,10, 'input /ma ディア <t>', 2 },
---        { 100, 10, 'input /ma ショックスパイク <me>', 5 },
-     },          
+    -- BLM は job/BLM.lua に移動
     RDM = {
         { 200, 300*2, 'input /ja コンポージャー <me>', 0 },
         { 100, 30, 'input /ma ディスペル <t>', 3 },
@@ -386,6 +355,18 @@ local sendCommandProbTableSub = {
 ---     { 100, 60, 'input /ja ファイターズロール  <me>', 1 },
     },
 }
+
+for j,v in pairs(acjob.jobTable) do
+    if v.mainJobProbTable ~= nil then
+	sendCommandProbTable[j] = v.mainJobProbTable
+    end
+    if v.mainJobProbTable_2 ~= nil then
+	sendCommandProbTable[j.."_2"] = v.mainJobProbTable_2
+    end
+    if v.subJobProbTable ~= nil then
+	sendCommandProbTableSub[j] = v.subJobProbTable
+    end
+end
 
 local isBacklineJob = function(job)
     if job == 'WHM' or job == 'RDM' or
