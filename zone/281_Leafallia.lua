55ef4df86439ae5a1303ed28a56da86885c4fcb6
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-12 21:13:32 +0900

    routes の zone への分離 230,236,237,238,240,242,243,244,245,252 256,257,258,261,262,263,265,266,267,268,270,274,277,281

---
+++ zone/281_Leafallia.lua
@@ -0,0 +1,13 @@
+-- リファーリア
+
+local M = { id = 281 }
+
+M.routes = {
+    -- HP#1
+    aged = { -- Aged Stump
+	{x=4.6,y=8.2,z=-0.3}, {x=-1.7,y=9.1},
+	{x=-25.7,y=32}, {a="f8touch"}
+    },
+}
+
+return M
