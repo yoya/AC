7174540c0b3686b78ec4e3f6582973035cdbff36
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-16 12:41:52 +0900

    ログイン時や reload 時に zone_change.warp_handler を呼ばないようガード。

--- incoming.lua
+++ incoming.lua
@@ -9,8 +9,10 @@ local prevZone = nil
 function incoming_handler()
     local pos = acpos.currentPos()
     local zone = windower.ffxi.get_info().zone
-    if prevZone ~= zone or acpos.distance(pos, prevPos) > 100 then
-        zone_change.warp_handler(zone, pos, prevZone, prevPos)
+    if prevPos ~= nil and prevZone ~= nil then
+	if prevZone ~= zone or acpos.distance(pos, prevPos) > 100 then
+	    zone_change.warp_handler(zone, pos, prevZone, prevPos)
+	end
     end
     prevZone = zone
     prevPos = pos
