49bdf41799497bc04e5eb51433e144c130546f32
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-18 00:47:41 +0900

    機能追加予定。 aim : 攻略中のコンテンツ state:行動を判断する為の状態

---
+++ aim.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+-- Idle, Leveling, Ambus, Works, Mission
+M.Idle = 0
+M.Leveling = 1
+M.Ambus = 2
+M.Works = 4
+M.Mission = 8
+M.allAims = {M.Leveling, M.Ambus, M.Works, M.Mission}
+
+M.aim = M.Idle
+
+return M
---
+++ state.lua
@@ -0,0 +1,8 @@
+-- 行動を判断する為の状態
+-- 最後に WS をうった時間とか。
+
+local M = {}
+
+M.checkStatus = function()
+    
+end
