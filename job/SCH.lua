fd54253a2fe7a573f0ce295e321785ab6e45cd8f
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-19 14:37:14 +0900

    学者の行動を最適化。

--- job/SCH.lua
+++ job/SCH.lua
@@ -2,16 +2,15 @@
 
 local M = {}
 
+local enhance = 'input /ja 令狸執鼠の章 <me>; wait 1; input /ja 女神降臨の章 <me>; wait 1'
+
 M.mainJobProbTable = {
-    { 100, 3600, 'input /ja 白のグリモア <me>', 0 },
-    { 100, 3600, 'input /ja 白の補遺 <me>', 0 },
-    { 100, 300/2, 'input /ja 女神降臨の章 <me>; wait 1; input /ma ストンスキン <me>', 8 },
-    { 100, 120, 'input /ja 女神降臨の章 <me>; wait 1; input /ma リジェネV <me>', 8 },
-    { 100, 120, 'input /ma リジェネV <p1>', 5 },
-    { 100, 120, 'input /ma 悪事千里の策 <p1>', 5 },
-    { 100, 120, 'input /ma 暗中飛躍の策 <p2>', 5 },
-    { 100, 120, 'input /ma 鼓舞激励の策 <p1>', 5 },
-    { 100, 120, 'input /ma 鼓舞激励の策 <p2>', 5 },
+    { 1000, 3600, 'input /ja 白のグリモア <me>; wait 2 ; input /ja 白の補遺 <me>', 3, true },
+    { 50, 120*2, enhance..'; input /ma ストンスキン <me>', 10 },
+    { 100, 120*2, enhance..'; input /ma リジェネV <me>', 10 },
+    { 50, 120*2, enhance..'; input /ma 鼓舞激励の策 <me>', 10 },
+    { 100, 180-10, 'input /ma 悪事千里の策 <p1>', 5 },
+    { 100, 180-10, 'input /ma 暗中飛躍の策 <p2>', 5 },
     { 50, 120, 'input /ja 机上演習 <me>', 0 },
     -- { 100, 30, 'input /ma サンダーII <t>', 2},
     -- { 200, 30, 'input /ma サンダーIII <t>', 3},
