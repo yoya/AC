b25e7440e6a03a3cc38a8a356750c59d557c6038
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-21 01:18:26 +0900

    暗黒は盾ジョブがしっかりしていないとPTが崩壊するので、一旦外す。

--- job/DRK.lua
+++ job/DRK.lua
@@ -12,7 +12,7 @@ M.mainJobProbTable = {
     { 100, 120, 'input /ma アブゾデック <t>', 3, true },
     { 100, 120, 'input /ma アブゾアキュル <t>', 3, true },
     { 100, 3000, 'input /ja ラストリゾート <me>', 0 },
-    { 100, 3000, 'input /ja 暗黒 <me>', 0 },
+    -- { 100, 3000, 'input /ja 暗黒 <me>', 0 },
     { 100, 3000, 'input /ja ディアボリクアイ <me>', 0 },
     { 100, 180, 'input /ja コンスームマナ <me>', 0 },
     { 100, 90, 'input /ja レッドデリリアム <me>', 0 },
