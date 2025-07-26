5d8b5e963956403c1ce80b2eee12a34305b9c39b
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-26 13:39:35 +0900

    io_chat/io_console 改善。文字列は加工せずにそのまま表示

--- io/chat.lua
+++ io/chat.lua
@@ -3,7 +3,7 @@ local M = {}
 local utils_table = require "utils/table"
 
 function M.print(v)
-    local text = utils_table.tableToString(v)
+    local text = type(v) == "string" and v or utils_table.tableToString(v)
     windower.add_to_chat(17, windower.to_shift_jis(text))
 end
 
--- io/console.lua
+++ io/console.lua
@@ -3,7 +3,7 @@ local M = {}
 local utils_table = require "utils/table"
 
 function M.print(v)
-    local text = utils_table.tableToString(v)
+    local text = type(v) == "string" and v or utils_table.tableToString(v)
     print(text)
 end
 
