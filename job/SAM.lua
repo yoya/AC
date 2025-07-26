f286549fdf456fbe9f1f897ee4d2bf4a12ed53cd
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-26 10:07:20 +0900

    ja の duration を 1 から 3 に。

--- job/SAM.lua
+++ job/SAM.lua
@@ -3,17 +3,17 @@
 local M = {}
 
 M.mainJobProbTable = {
-    { 100, 180, 'input /ja 黙想 <me>', 1 },
-    { 200, 60, 'input /ja 八双 <me>', 1 }, -- 攻撃
-    -- { 100, 60, 'input /ja 星眼 <me>', 1 }, -- 防御
-    { 100, 180, 'input /ja 先義後利 <me>', 1 },
-    { 100, 180, 'input /ja 渾然一体 <me>', 1 },
-    { 100, 180, 'input /ja 石火之機 <me>', 1 },
+    { 100, 180, 'input /ja 黙想 <me>', 3 },
+    { 200, 60, 'input /ja 八双 <me>', 3 }, -- 攻撃
+    -- { 100, 60, 'input /ja 星眼 <me>', 3 }, -- 防御
+    { 100, 180, 'input /ja 先義後利 <me>', 3 },
+    { 100, 180, 'input /ja 渾然一体 <t>', 3 },
+    { 100, 180, 'input /ja 石火之機 <me>', 3 },
 }
 
 M.subJobProbTable = {
-    { 60, 180, 'input /ja 黙想 <me>', 1 },
-    { 200, 60, 'input /ja 八双 <me>', 1 }, -- 攻撃
+    { 60, 180, 'input /ja 黙想 <me>', 3 },
+    { 200, 60, 'input /ja 八双 <me>', 3 }, -- 攻撃
     -- { 60, 60, 'input /ja 星眼 <me>', 1 }, -- 防御
     { 100, 180, 'input /ja 石火之機 <me>', 1 },
 }
