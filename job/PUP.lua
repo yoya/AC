b6d25673f700e2b15cb1cc63633e93cfe1c626e8
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-26 10:06:10 +0900

    アクティベートやデプロイが、敵と戦い始めたらなるべく早めに使う

--- job/PUP.lua
+++ job/PUP.lua
@@ -3,12 +3,12 @@
 local M = {}
 
 M.mainJobProbTable = {
-    { 100, 1200, 'input /ja アクティベート <me>', 0 },
-    { 100, 60, 'input /ja 応急処置 <me>', 0 },
-    { 500, 60, 'input /pet ディプロイ <t>', 0, true },
-    { 200, 300, 'input /ja クールダウン <me>', 0 },
-    { 200, 90, 'input /pet ファイアマニューバ <me>', 0 },
-    { 100, 90, 'input /pet ライトマニューバ <me>', 0 },
+    { 200, 1200, 'input /ja アクティベート <me>', 3, true},
+    { 100, 60, 'input /ja 応急処置 <me>', 3 },
+    { 1000, 20, 'input /pet ディプロイ <t>', 3, true },
+    { 500, 300, 'input /ja クールダウン <me>', 3 },
+    { 200, 90, 'input /pet ファイアマニューバ <me>', 3 },
+    { 100, 90, 'input /pet ライトマニューバ <me>', 3 },
 }
 
 M.subJobProbTable = { }
