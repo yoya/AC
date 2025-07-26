e90325b50a6f1576be5908c58116efce01d8b609
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-26 10:05:15 +0900

    集中の頻度を増やす かまえる。追加。 無双無念を削除。(無闇に使ってもオートアタックの回数が減るだけ)

--- job/MNK.lua
+++ job/MNK.lua
@@ -3,11 +3,13 @@
 local M = {}
 
 M.mainJobProbTable = {
-    { 60, 180, 'input /ja 集中 <me>', 0 },
+    { 100, 120, 'input /ja 集中 <me>', 0 },
     { 60, 180, 'input /ja チャクラ <me>', 0 },
     { 60, 600, 'input /ja マントラ <me>', 0 },
     { 60, 300, 'input /ja 猫足立ち <me>', 0 },
+    { 60, 300, 'input /ja かまえる <me>', 0 },
     { 60, 300, 'input /ja インピタス <me>', 0 },
+    -- { 60, 600, 'input /ja 無想無念 <me>', 0 },
 }
 
 M.subJobProbTable = {
