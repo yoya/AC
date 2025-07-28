53e4db61354a9b98a3a994aa38523ced92188f58
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-29 03:11:41 +0900

    可変長引数での print に対応

--- io/chat.lua
+++ io/chat.lua
@@ -2,8 +2,16 @@ local M = {}
 
 local utils_table = require "utils/table"
 
-function M.print(v)
-    local text = type(v) == "string" and v or utils_table.tableToString(v)
+function M.print(...)
+    local text = ""
+    for i, v in pairs({...}) do
+	local t = type(v) == "string" and v or utils_table.tableToString(v)
+	if i == 1 then
+	    text = text .. t
+	else
+	    text = text .. " " .. t
+	end
+    end
     windower.add_to_chat(17, windower.to_shift_jis(text))
 end
 
