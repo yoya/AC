020699c4eab3298bf50d3c06e12d7db4ee1ad0e9
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-30 15:20:16 +0900

    utils/party から inspect にアクセスできるよう参照を繋げる

--- inspect.lua
+++ inspect.lua
@@ -1,10 +1,13 @@
 -- 行動を判断する為の監視データ
 -- 最後に WS をうった時間とか。
 
+local utils = require 'utils'
+
 local M = {
     ws_time = 0,
     eminence_point = 0,
 }
+utils.inspect = M
 
 function M.ws()
     M.ws_time = os.time()
