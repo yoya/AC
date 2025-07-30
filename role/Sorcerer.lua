bfbe52a1051178394e5161a00e6047dd6ccd862b
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-30 19:46:44 +0900

    魔法名を変数にして、まとめて切り替えられるように。

--- role/Sorcerer.lua
+++ role/Sorcerer.lua
@@ -1,20 +1,27 @@
 -- MagicBurst 役。黒魔道士/赤魔導士
 
+local M = {}
+
 local command = require 'command'
 local asinspect = require 'inspect'
 
-local M = {}
+-- local MB_magic = "ファイア"
+local MB_magic = "ブリザド"
 
 function M.mainTick(player)
-    local ws_time = asinspect.ws_time + 4
-    local now = os.time()
-    if ws_time < now then
-	if  now < (ws_time + 2) then
-	    -- command.send('input /ma ブリザドV <t>')
-	    command.send('input /ma ファイアV <t>')
-	elseif now < (ws_time + 4) then
-	    -- command.send('input /ma ブリザドIV <t>')
-	    command.send('input /ma ファイアIV <t>')
+    if player.status == 1 then -- 戦闘中
+	local ws_time = asinspect.ws_time
+	local now = os.time()
+	if ws_time + 4 < now then
+	    if  now < (ws_time + 5) then
+		command.send('input /ma '..MB_magic..'V <t>')
+	    elseif now < (ws_time + 6) then
+		command.send('input /ma '..MB_magic..'IV <t>')
+	    elseif now < (ws_time + 8) then
+		command.send('input /ma '..MB_magic..'III <t>')
+	    elseif now < (ws_time + 10) then
+		command.send('input /ma '..MB_magic..'II <t>')
+	    end
 	end
     end
 end
