601d08380f7bd9e331113e037cb67d93253ba04a
Yoshihiro Yamazaki <yoya@awm.jp>
2025-07-31 16:47:16 +0900

    stat メッセージ色をつける。 chat を require すると string:color が使える。

--- stat.lua
+++ stat.lua
@@ -1,6 +1,7 @@
 -- 統計情報。
 -- 倒した敵の count とか
 
+require('chat') -- string:color
 local io_chat = require('io/chat')
 
 local M = {
@@ -19,7 +20,7 @@ function M.defeat(name)
 end
 
 function M.print()
-    io_chat.print("=== Defeated Enemy table ===");
+    io_chat.print("=== Defeated Enemy Table ===":color(5));
     for name, count in pairs(M.DefeatedEnemyTable) do
 	io_chat.print(name .. ": "..count);
     end
