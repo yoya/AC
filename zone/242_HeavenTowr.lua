21ecc4723a7e9474132fb03d12c1490a00a3726e
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-04 21:51:21 +0900

    天の塔に入ったら kupipi まで自動で移動。

--- zone/242_HeavenTowr.lua
+++ zone/242_HeavenTowr.lua
@@ -3,9 +3,20 @@
 local M = { id = 242 }
 
 M.routes = {
-    kupipi = { {x=0,y=-22.4,z=0}, {x=-10.1,y=1.7},
-	{x=-0.6,y=19.5}, {x=1.7,y=30}, 
+    kupipi = {
+	{x=0,y=-22.4,z=0}, {x=-10,y=-4}, {x=-10,y=7},
+	{x=-1,y=20.5}, {a="opendoor"}, {x=0,y=25},
+	{x=1.5,y=30.1,z=0}, {a="f8touch"}
     },
+}
+
+M.essentialPoints = {
+    from_win = {x=0,y=-22.4,z=0},
+
+}
+
+M.automatic_routes = {
+    from_win = "kupipi",
 }
 
 return M
