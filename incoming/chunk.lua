5bc9558df74966b80028f95a533e4bae5f78a23b
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-05 10:03:09 +0900

    戦闘終了時の処理を ac/defeated.lua に移動。 こまごまとした整理。 マスターレベル情報を追加しようとしたが、うまくいってない。

--- incoming/chunk.lua
+++ incoming/chunk.lua
@@ -1,14 +1,29 @@
+local M = {}
+
 local packets = require('packets')
 
 local utils = require('utils')
 local io_chat = require('io/chat')
 local acstat = require('stat')
 local acinspect = require('inspect')
+local ac_record =  require('ac/record')
+local ac_defeated = require('ac/defeated')
 
-local M = {}
+local packet_handler = { }
 
-function actionHandler(amPacket) -- 0x028
-    local cate = amPacket.Category
+packet_handler[0x01B] = function(packet) -- Job Info
+    --[[
+    local data = packet._raw
+    local arr = data:unpack('I')
+    local main_job = arr[0x08]
+    print("main_job"..main_job)
+    local job_master_level = arr[0x6D+(2*main_job)+1]
+    io_chat.print("job_master_level:"..job_master_level)
+    ]]
+end
+
+packet_handler[0x028] = function(packet) -- Action Message
+    local cate = packet.Category
     -- io_chat.print("0x028: " .. cate)
     if cate == 1 then
 	-- auto attack
@@ -22,47 +37,61 @@ function actionHandler(amPacket) -- 0x028
     end
 end
 
-function actionMessageHandler(amPacket) -- 0x029
-    -- io_chat.print(amPacket)
-    local mesg = amPacket.Message
+packet_handler[0x029] = function(packet) -- Action Message
+    -- io_chat.print(packet)
+    local mesg = packet.Message
     if mesg == 6 then  -- defeated enemy
-	-- io_chat.print(amPacket)
-	local actor_index = amPacket["Actor Index"]
-	local target_index = amPacket["Target Index"]
+	-- io_chat.print(packet)
+	local actor_index = packet["Actor Index"]
+	local target_index = packet["Target Index"]
 	-- io_chat.print("defeated enemy: actor:"..actor_index .. " target:"..target_index)
+	-- 敵を倒した時の処理
 	if utils.isMemberIndex(actor_index) then
 	    local mob = windower.ffxi.get_mob_by_index(target_index)
 	    acstat.defeat(mob.name)
-	    acstat.print()
+	    -- defeated 表示/保存処理は queue に乗せる予定
+	    ac_defeated.done()
 	end
     elseif mesg == 20 then
+	io_chat.setNextColor(3)
 	io_chat.print("XXX: action message: Fall???")
     elseif mesg == 206 then
 	do end -- (味方の？)強化切れ
     else
 	do end
-	-- io_chat.print("XXX 0x029: "..amPacket.Message)
+	-- io_chat.print("XXX 0x029: "..packet.Message)
 	-- 4 敵弱体
 	-- 17 味方強化？
 	-- 18  MB ？
     end
 end
 
+-- Kill Message, when you gain XP/LP/CP/JP/MP
+packet_handler[0x02D] = function(packet)
+    --local player = windower.ffxi.get_player()
+    --io_chat.print("============== packet(0x2D)")
+    --if packet["Player Index"] == player.index then
+    --    io_chat.print(packet)
+    --end
+end
+
+-- Char Stats -- id がないので(パーティメンバーでなく)自分のみの情報？
+packet_handler[0x061] = function(packet)
+    local points = packet["Unity Points"]  -- ユニテイポイント
+    ac_record.unity(points)
+end
+
+-- Update Current Sparks via 110
+packet_handler[0x110] = function(packet)
+    local sparks = packet["Sparks Total"]
+    ac_record.eminence(sparks)
+end
+
 function M.incoming_handler(id, data, modified, injected, blocked)
-    local player = windower.ffxi.get_player()
-    if id == 0x028 then -- Action Message
-	actionHandler(packets.parse('incoming', data))
-    elseif id == 0x029 then -- Action Message
-	actionMessageHandler(packets.parse('incoming', data))
-    elseif id == 0x02D then
-	--        local packet = packets.parse('incoming', data)
-	--          io_chat.print("============== packet(0x2D)")
-	--        if packet["Player Index"] == player.index then
-	--            io_chat.print(packet)
-	--        end
-    elseif id == 0x110 then -- Update Current Sparks via 110
-	local header, value1, value2, Unity1, Unity2, Unknown = data:unpack('II')
-	acinspect.eminence(value1)
+    local handler = packet_handler[id]
+    if handler ~= nil then
+	local packet = packets.parse('incoming', data)
+	handler(packet)
     end
 end
 
