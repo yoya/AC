2e67e7dbfd4df03f76fa7e04543f667e8409eb2a
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-06 00:22:54 +0900

    調整。RDM がリーダーの場合、２つ目はアタッカーで、3,4人目に後衛がくる事が多いので、 その前提にする。

--- job/RDM.lua
+++ job/RDM.lua
@@ -15,16 +15,18 @@ M.mainJobProbTable = {
     { 100, 180*3, 'input /ma ストライII <me>', 4 },
     { 100, 300*3, 'input /ma ゲインデック <me>', 4 },
     { 100, 150*3, 'input /ma リフレシュIII <me>', 4 },
-    { 100, 150, 'input /ma リフレシュIII <p1>', 4 },
     { 100, 150, 'input /ma リフレシュIII <p2>', 4 },
+    { 100, 150, 'input /ma リフレシュIII <p3>', 4 },
     { 100, 180, 'input /ma エンサンダーII <me>', 4 },
     -- { 100, 180, 'input /ma エンストーンII <me>', 4 },
     { 100, 180*3, 'input /ma ヘイストII <me>', 3 },
     { 100, 180, 'input /ma ヘイストII <p1>', 3 },
+    { 100, 180, 'input /ma ヘイストII <p2>', 3 },
     { 100, 180, 'input /ma ヘイストII <p3>', 3 },
     { 5, 600, 'input /ma アクアベール <me>', 5},
     { 5, 300, 'input /ma ブリンク <me>', 5},
     { 5, 300, 'input /ma ストンスキン <me>', 5},
+    { 600/2, 1000, 'input /ja コンバート <me>', 1 },
 }
 
 M.subJobProbTable = {
@@ -36,7 +38,7 @@ M.subJobProbTable = {
     { 500, 120, 'input /ma ディアII <t>', 4, true },
     { 500, 120, 'input /ma ディストラ <t>', 5, true },
     { 500, 120, 'input /ma フラズル <t>', 5, true },
-    { 600/2, 1000, 'input /ja コンバート <me>', 1 },
+    { 600/3, 1000, 'input /ja コンバート <me>', 1 },
     { 500, 120-10, 'input /ma ヘイスト <p1>', 4 },
     { 500, 120-10, 'input /ma ヘイスト <p2>', 4 },
     -- { 100, 120-30, 'input /ma ヘイスト <p3>', 4 },
