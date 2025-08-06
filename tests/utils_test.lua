ecad80e4a3b28f6585faffce18fa1f2cba1797c3
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-06 18:00:33 +0900

    utils.angle のテストを追加。

--- tests/utils_test.lua
+++ tests/utils_test.lua
@@ -1,5 +1,11 @@
 package.path = package.path .. ";../?.lua"
 local utils = require "utils"
 local get_keys = utils.table.get_keys
+local midangle = utils.angle.midangle
 
-print(get_keys({A="123", X="789"})[2])
+print("get_keys:", get_keys({A="123", X="789"})[2])
+
+for i = 0, 7 do
+    local t = 2*math.pi*(i/8)
+    print("midangle:", i, t, midangle(t, t + 2*math.pi / 8))
+end
