4602b2232ded3249e451d79dacf3c64ed71a0096
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-11 21:07:43 +0900

    ファイル記録に、next job point を追加。

--- ac/record.lua
+++ ac/record.lua
@@ -33,7 +33,20 @@ function M.record()
 	f:write(" Ilv:"..player.item_level)
 	local jobpt = player.job_points[string.lower(player.main_job)]
 	if jobpt.jp_spent < 2100 then
+	    -- ジョブポイント表示
 	    f:write(" JP:"..jobpt.jp_spent.."+"..jobpt.jp.."="..(jobpt.jp_spent+jobpt.jp))
+	    -- 次にキャパポupする jobポイントの閾値を表示する
+	    local cp_jp_list = {5,25,55,95,145,205,275,355,445,545,655,775,905,1045,1195,1355,2100}
+	    local prev_jp = 0
+	    local next_jp = 5
+	    for i, p in ipairs(cp_jp_list) do
+		if prev_jp <= jobpt.jp_spent and jobpt.jp_spent < p then
+		    next_jp = p
+		    break
+		end
+		prev_jp = p
+	    end
+	    f:write("(next:"..next_jp..")")
 	elseif jobpt.jp_spent >= 2100 and ac_char.real_master_level() >= 0 then
 	    f:write(" Mlv:"..ac_char.real_master_level())
 	    local exemplar_percent = math.floor(100 * ac_char.current_exemplar_point()/ac_char.next_exemplar_point() + 0.5)
