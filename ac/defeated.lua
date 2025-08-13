e804c3791b164618c6edd22eccea3685a4b7687f
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-14 01:02:12 +0900

    戦闘後のジョブポイント情報表示。色の変更と ### の数を減らす。

--- ac/defeated.lua
+++ ac/defeated.lua
@@ -16,12 +16,14 @@ function M.done()
 	local cp_jp_list = {5,25,55,95,145,205,275,355,445,545,655,775,905,1045,1195,1355,2100}
 	for i, p in ipairs(cp_jp_list) do
 	    if jobpt.jp_spent < p and p <= jobpt.jp_spent+jobpt.jp then
-		io_chat.setNextColor(3)
-		io_chat.print("======= job point: "..p.."(clear) <= "..jobpt.jp_spent.."+"..jobpt.jp.."="..(jobpt.jp_spent+jobpt.jp))
+		-- io_chat.setNextColor(3) -- オレンジ
+		-- io_chat.setNextColor(4) -- ピンク
+		io_chat.setNextColor(8) -- 明るい赤紫
+		io_chat.print("=== job point: "..p.."(clear) <= "..jobpt.jp_spent.."+"..jobpt.jp.."="..(jobpt.jp_spent+jobpt.jp))
 	    end
 	    if jobpt.jp_spent+jobpt.jp < p then
-		io_chat.setNextColor(3)
-		io_chat.print("======= job point: "..jobpt.jp_spent.."+"..jobpt.jp.."="..(jobpt.jp_spent+jobpt.jp).." < "..p.."(next)")
+		io_chat.setNextColor(6)
+		io_chat.print("=== job point: "..jobpt.jp_spent.."+"..jobpt.jp.."="..(jobpt.jp_spent+jobpt.jp).." < "..p.."(next)")
 		break
 	    end
 	end
