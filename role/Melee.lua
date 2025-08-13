6a5e80e2cda0cfb587c69e9fca33bfc0f20728cb
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-14 01:03:25 +0900

    戦闘中だけ食事をする。(普段歩いてる時に食事されても困る)

--- role/Melee.lua
+++ role/Melee.lua
@@ -5,11 +5,13 @@ local M = {}
 local task = require 'task'
 
 function M.main_tick(player)
-    local c = "input /item カルボナーラ <me>"
-    local level = task.PRIORITY_LOW
-    -- command, delay, duration, period, eachfight
-    local t = task.newTask(c, 1, 3, 30*60/3, false)
-    task.setTask(level, t)
+    if player.status == 1 then
+	local c = "input /item カルボナーラ <me>"
+	local level = task.PRIORITY_LOW
+	-- command, delay, duration, period, eachfight
+	local t = task.newTask(c, 1, 3, 30*60/10, false)
+	task.setTask(level, t)
+    end
 end
 
 return M
