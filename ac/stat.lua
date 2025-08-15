ab0433176353f8194d801d4d551e69ea54b26713
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-15 12:10:34 +0900

    stat の倒した敵集計表示。 まだ一匹も倒してない時は控えめに表示する。

--- ac/stat.lua
+++ ac/stat.lua
@@ -18,17 +18,29 @@ function M.defeat(name)
     M.DefeatedEnemyTable[name] = M.DefeatedEnemyTable[name] + 1
 end
 
+function tableHasData(t)
+    for k, v in pairs(t) do
+	return true
+    end
+    return false
+end
+
 function M.print()
     local total = 0
-    io_chat.setNextColor(5)
-    io_chat.print("=== Defeated Enemy Table ===");
-    for name, count in pairs(M.DefeatedEnemyTable) do
-	io_chat.setNextColor(6)
-	io_chat.print(name .. ": "..count);
-	total = total + count
+    -- if #M.DefeatedEnemyTable == 0 then -- 駄目
+    if tableHasData(M.DefeatedEnemyTable) == false then
+	io_chat.print("=== Defeated Enemy Zero ===");
+    else
+	io_chat.setNextColor(5)
+	io_chat.print("=== Defeated Enemy Table ===");
+	for name, count in pairs(M.DefeatedEnemyTable) do
+	    io_chat.setNextColor(6)
+	    io_chat.print(name .. ": "..count);
+	    total = total + count
+	end
+	io_chat.setNextColor(5)
+	io_chat.print("=> Total: "..total);
     end
-    io_chat.setNextColor(5)
-    io_chat.print("=> Total: "..total);
 end
 
 return M
