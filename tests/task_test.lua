00ca9dfe7c67e19740693197cf79203932fc3597
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-15 21:33:07 +0900

    task モジュールテストに優先度と remove を追加。

--- tests/task_test.lua
+++ tests/task_test.lua
@@ -3,17 +3,19 @@ local actask = require('task')
 local io_console = require('io/console')
 
 local task1 = actask.newTask("input /ja 挑発 <t>", 0, 2, 60, false)
-local task2 = actask.newTask("input /ma フラッシュ <t>", 0, 2, 45, false)
-local task3 = actask.newTask("input /ma スタン <t>", 0, 2, 10, false)
-local task4 = actask.newTask("input /echo これは無し", 1, 0, 0, false)
-local task5 = actask.newTask("input /ma ウォークライ <t>", 0, 2, 300, false)
+local task2 = actask.newTask("input /echo 消されるタスク", 0, 2, 45, false)
+local task3 = actask.newTask("input /ma ウォークライ <t>", 0, 2, 300, false)
+local task4 = actask.newTask("input /echo 1秒後に実行", 1, 0, 0, false)
+local task5 = actask.newTask("input /ma スタン <t>", 0, 2, 10, false)
 
 actask.setTask(actask.PRIORITY_HIGH, task1)
 actask.setTask(actask.PRIORITY_HIGH, task1)
 actask.setTask(actask.PRIORITY_HIGH, task2)
-actask.setTask(actask.PRIORITY_TOP, task3)
+actask.setTask(actask.PRIORITY_LOW, task3)
 actask.setTask(actask.PRIORITY_MIDDLE, task4)
-actask.setTask(actask.PRIORITY_MIDDLE, task5)
+actask.setTask(actask.PRIORITY_TOP, task5)
+
+actask.removeTask(actask.PRIORITY_HIGH, task2)
 
 for i = 1,3 do
     print("tick:", i, "time:"..os.time())
