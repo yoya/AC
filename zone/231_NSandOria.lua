396310d1572b7ad26154f099f9d606c948e22725
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-17 21:39:24 +0900

    zone 101, 166, 231 追加。

---
+++ zone/231_NSandOria.lua
@@ -0,0 +1,21 @@
+-- 北サンドリア
+
+local M = { id = 231 }
+
+M.routes = {
+    -- HP#2
+    castle = {  -- 城
+	{x=10,y=94,z=-0.2}, {x=6,y=99}, {x=5,y=107}, {}
+    },
+}
+
+M.essentialPoints = {
+    hp2 = {x=10,y=94,z=-0.2},
+}
+
+M.automatic_routes = {
+    hp2 = "castle"
+}
+
+
+return M
