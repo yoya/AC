81f1aa1aae875575aee687014315bd5042105f3e
Yoshihiro Yamazaki <yoya@awm.jp>
2025-08-24 00:13:12 +0900

    command の refine

--- command.lua
+++ command.lua
@@ -3,10 +3,9 @@
 local M = {}
 
 -- 直接 send_command を呼ぶと文字化けするのでラッパーを用意
-function send(c)
+function M.send(c)
     s = windower.to_shift_jis(c)
     windower.send_command(s)
 end
-M.send = send
 
 return M
