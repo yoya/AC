e2e52bcb20e16e85f199e5582725bcbf4fe86f8b
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-01 23:57:28 +0900

    かえるの池だと防御歌。他は攻撃歌で分ける。 詩人も Melee としてカルボナーラを食べる。

--- job/BRD.lua
+++ job/BRD.lua
@@ -2,21 +2,12 @@
 
 local M = {}
 
+local task = require 'task'
+local role_Melee = require 'role/Melee'
+local aczone = require('zone')
+local io_chat = require('io/chat')
+
 M.mainJobProbTable = {
-    --[[
-	{ 100, 120, 'input /ma 重装騎兵のミンネIV <me>', 8 },
-        { 200, 120, 'input /ma 重装騎兵のミンネV <me>', 8 },
-        { 200, 120, 'input /ma 闘龍士のマンボ <me>', 8 },
-        { 200, 120, 'input /ma 活力のエチュード <me>', 8 },
-        { 200, 120, 'input /ma 戦士達のピーアンVI <me>', 8 },
-    ]]
-    { 150, 900/2, 'input /ma 無敵の進撃マーチ <me>', 8 },
-    { 300, 900/3, 'input /ma 栄光の凱旋マーチ <me>', 8 },
-    { 250, 900/2, 'input /ma 猛者のメヌエットV <me>', 8 },
-    { 50, 900, 'input /ma 猛者のメヌエットIV <me>', 8 },
-    { 100, 900, 'input /ma 剣豪のマドリガル <me>', 8 },
-    { 10, 900, 'input /ma 怪力のエチュード <me>', 8 },
-    { 100, 900/2, 'input /ma 妙技のエチュード <me>', 8 },
     -- { 100, 60, 'input /ma 魔法のフィナーレ <t>', 8, true },
     -- { 200, 120, 'input /ma 修羅のエレジー <t>', 8, true },
     { 1000, 120, 'input /ma 魔物のレクイエムVII <t>', 8, true },
@@ -29,8 +20,44 @@ M.subJobProbTable = {
     -- { 200, 120, 'input /ma 戦場のエレジー <t>', 8, true },
 }
 
+function song(song_name, onoff, delay)
+    local c = "input /song "..song_name.." <me>"
+    local level = task.PRIORITY_MIDDLE
+    local period = 15*60 / 3
+    -- command, delay, duration, period, eachfight
+    local t = task.newTask(c, delay, 10, period, true)
+    if onoff then
+	task.setTask(level, t)
+    else
+	task.removeTask(level, t)
+    end
+end
+
+function song_tick(player)
+    local zone = windower.ffxi.get_info().zone
+    local onoff = player.status > 0
+    if aczone.isNear(291, "toad_pond", 120) then
+	song("重装騎兵のミンネV", onoff, 2)
+	song("闘龍士のマンボ", onoff, 12)
+	-- song("活力のエチュード", onoff, 12*2)
+	song("戦士達のピーアンVI", onoff, 12*3)
+	song("栄光の凱旋マーチ", onoff, 12)
+	return
+    end
+    -- TODO auto かつ街中以外で以下を実行。status 1 で弱体系実行
+    song("無敵の進撃マーチ", onoff, 12)
+    song("栄光の凱旋マーチ", onoff, 12*2)
+    song("猛者のメヌエットV", onoff, 12*3)
+    -- song("剣豪のマドリガル", onoff, 12*4)
+    -- song("怪力のエチュード", onoff, 12*4)
+    song("妙技のエチュード", onoff, 12*4)
+end
+
 function M.main_tick(player)
-    do end
+    if role_Melee.main_tick ~= nil then
+	role_Melee.main_tick(player)
+    end
+    song_tick(player)
 end
 
 return M
