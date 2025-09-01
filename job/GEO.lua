4ac364fbe90ef6a888bbb3d7cb55f32fe6aec12f
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-02 01:39:21 +0900

    風水魔法実行の整理。 ジオとインデ魔法の使い分け処理。主に醴泉島のかえる対応。 状況に応じて羅盤解除のアビリティを使い分ける

--- job/GEO.lua
+++ job/GEO.lua
@@ -5,69 +5,81 @@ local M = {}
 local command = require 'command'
 local role_Sorcerer = require 'role/Sorcerer'
 
+local aczone = require 'zone'
 local ac_pos = require 'ac/pos'
 local io_chat = require 'io/chat'
 local task = require 'task'
 
---- 通常の敵
---local GEO_inde = "インデフューリー"
-local GEO_inde = "インデヘイスト"
-local GEO_geo = "ジオフレイル"  -- 防御down
-local GEO_inde2 = "インデヒューリー" -- 攻撃力up
-local GEO_geo2 = "ジオトーパー" -- 回避down
-
---- 格上の敵
---local GEO_inde = "インデプレサイス" --- 命中up
---- 醴泉島かえる
---local GEO_inde = "インデリジェネ"
---local GEO_geo = "ジオバリア"  -- 防御down
-
---- アンバス用
---- local GEO_inde = "インデバリア" --- アンバス物防up
----local GEO_geo = "ジオアトゥーン" --- 魔回避down
----local GEO_inde = "インデフェンド" --- アンバス魔防up
---- カオス戦用
---- local GEO_inde = "インデフェンド" --- アンバス魔防up
-
 M.mainJobProbTable = {
-    { 100, 120, 'input /ma ケアル <p1>', 4 },
+    { 100, 120, 'input /ma ケアル <p1>', 4, true },
     { 100, 120, 'input /ma ヘイスト <p2>', 6 },
-    --{ 50, 30, 'input /ma サンダー <t>', 3},
-    --{ 50, 30, 'input /ma サンダーII <t>', 3},
-    -- { 100, 30, 'input /ma サンダーIII <t>', 3},
-    --{ 50, 30, 'input /ma ファイア <t>', 3},
-    --{ 50, 30, 'input /ma  ファイアII <t>', 3},
-    -- { 100, 30, 'input /ma ファイアIII <t>', 3},
-    --{ 50, 30, 'input /ma ブリザド <t>', 3},
-    --{ 50, 30, 'input /ma ブリザドII <t>', 3},
-    -- { 100, 30, 'input /ma ブリザドIII <t>', 3},
 }
 
-M.mainJobProbTable_1 = {
-    { 150, 300/3, 'input /ma '..GEO_geo..' <t>; wait 1; input /ja サークルエンリッチ <me>', 6 },
-    -- { 250, 20, 'input /ma '..GEO_geo..' <t>', 6 },
-    -- { 50, 600/2, 'input /ja グローリーブレイズ <me>; wait 1; input /ja フルサークル <me>; wait 1; input /ma '..GEO_geo..' <t>', 12  },
-    { 150, 180/2, 'input /ma '..GEO_inde..' <me>', 4 },
-    { 10, 600, 'input /ja エントラスト <me>; wait 2; input /ma インデデック <p2>', 6 },
-    { 10, 300, 'input /ja メンドハレイション <me>; wait 2; input /ma '..GEO_geo..' <t>', 7 },
-    { 10, 300, 'input /ja レイディアルアルカナ <me>; wait 2; input /ma '..GEO_geo..' <t>', 7 },
-}
+function isDefensive()
+    if aczone.isNear(291, "toad_pond", 120) then
+	return true
+    end
+    return false
+end
 
-M.mainJobProbTable_2 = {
-    { 150, 300/2, 'input /ma '..GEO_geo2..' <t>; wait 1; input /ja サークルエンリッチ <me>', 6 },
-    { 250, 20, 'input /ma '..GEO_geo2..' <t>', 6 },
-    -- { 50, 600/2, 'input /ja グローリーブレイズ <me>; wait 1; input /ja フルサークル <me>; wait 1; input /ma '..GEO_geo2..' <t>', 12  },
-    { 150, 180, 'input /ma '..GEO_inde2..' <me>', 4 },
-    { 10, 600, 'input /ja エントラスト <me>; wait 2; input /ma インデスト <p2>', 6 },
-    { 10, 300, 'input /ja メンドハレイション <me>; wait 2; input /ma '..GEO_geo2..' <t>', 7 },
-    { 10, 300, 'input /ja レイディアルアルカナ <me>; wait 2; input /ma '..GEO_geo2..' <t>', 7 },
-}
- 
-M.subJobProbTable = { }
-
-function geo_setup()
+function inde_setup(jobRank)
+    local GEO_inde = "インデヘイスト"
+    local GEO_entrust = "インデデック"
+    if jobRank == 2 then
+	local GEO_inde = "インデフューリー"
+	local GEO_entrust = "インデスト"
+    end
+    if isDefensive() then
+	GEO_inde = "インデバリア"  -- 防御up
+	if jobRank == 2 then
+	    GEO_inde = "インデアトゥーン"  -- 魔回避up
+	end
+    end
+    -- TODO: MB パーティ対応
+    --   GEO_inde = "インデフォーカス" -- 魔命 up
+    --   GEO_inde = "インデアキュメン" -- 魔攻 up
+    -- TODO: 格上の敵対応
+    --   GEO_inde = "インデプレサイス" -- 命中up
     local level = task.PRIORITY_MIDDLE
-    local c = 'input /ja グローリーブレイズ <me>; wait 2; input /ma '..GEO_geo..' <bt>'
+    local c = 'input /ma '..GEO_inde..' <me>'
+    -- command, delay, duration, period, eachfight
+    local t = task.newTask(c, 2, 7, 180/2, false)
+    -- 戦闘開始で硬直してる可能性があるので、3秒待つ
+    local done = task.setTask(level, t)
+    if done then
+	io_chat.setNextColor(6)
+	io_chat.print("インデ展開")
+    end
+    -- エントラスト TODO:実行可能な時に狙って処理する
+    c = 'input /ja エントラスト <me>; wait 2; input /ma '..GEO_entrust..' <p2>'
+    -- command, delay, duration, period, eachfight
+    t = task.newTask(c, 2, 7, 600, false)
+    local done = task.setTask(level, t)
+    if done then
+	io_chat.setNextColor(6)
+	io_chat.print("エントラストインデ展開")
+    end
+end
+
+function geo_setup(jobRank)
+    -- print("geo_setup jobRank:"..jobRank)
+    local GEO_geo = "ジオフレイル"  -- 敵の防御down
+    if jobRank == 2 then
+	GEO_geo = "ジオトーパー" -- 敵の回避率down
+    end
+    if isDefensive() then
+	GEO_geo = "ジオウィルト"  -- 敵の攻撃力down
+	if jobRank == 2 then
+	    GEO_geo = "ジオフェイド" -- 敵の魔法攻撃力down
+	end
+    end
+    -- TODO: MB パーティ対応
+    --   GEO_geo = "ジオマレーズ" -- 魔防down
+    --   GEO_geo = "ジオアトゥーン" -- 魔回避down
+    -- TODO: 格上の敵対応
+    --   GEO_geo = "ジオトーパー" -- 敵の回避率down
+    local level = task.PRIORITY_MIDDLE
+    local c = 'input /ja グローリーブレイズ <me>; wait 2; input /ma '..GEO_geo..' <bt>; wait 2; input /ja サークルエンリッチ <me>'
     -- command, delay, duration, period, eachfight
     local t = task.newTask(c, 2, 10, 5, false)
     -- 戦闘開始で硬直してる可能性があるので、3秒待つ
@@ -78,9 +90,9 @@ function geo_setup()
     end
 end
 
-function geo_release()
+function geo_release(name)
     local level = task.PRIORITY_MIDDLE
-    local c = 'input /ja フルサークル <me>'
+    local c = 'input /ja '..name..' <me>'
     -- command, delay, duration, period, eachfight
     local t = task.newTask(c, 1, 2, 0, false)
     local done = task.setTask(level, t)
@@ -90,21 +102,34 @@ function geo_release()
     end
 end
 
+-- 状況に応じて羅盤解除
+function geo_release_with_contexte(player, pet, mob)
+    local mobpetdist = ac_pos.distance(pet, mob)
+    if mobpetdist >= 6 then
+	-- 羅盤が戦闘場所から離れてたら消す
+	io_chat.setNextColor(4) -- ピンク
+	local pd = math.floor(mobpetdist * 100 + 0.5) / 100;
+	io_chat.print("羅盤と敵の距離="..pd)
+	geo_release("フルサークル")
+    elseif player.vitals.mp < 100 then  -- TODO: パーティのMPを考慮
+	geo_release("レイディアルアルカナ")
+    elseif player.vitals.hp < 300 then  -- TODO: パーティのHPを考慮
+	geo_release("メンドハレイション")
+    end
+end
+
 function M.main_tick(player)
-    local pet = windower.ffxi.get_mob_by_target("<pet>")
+    local jobRank = 1  -- あとで party 情報を元に設定
     if player.status == 1 then -- 戦闘中
-	local mob = windower.ffxi.get_mob_by_target("<t>")
-	local mobpetdist = ac_pos.distance(pet, mob)
-	if mob ~= nil then
-	    -- 羅盤がなくて戦闘中の敵がいる時は設置する
-	    if pet == nil then
-		geo_setup()
-	    elseif mobpetdist > math.random(30, 40) then
-		-- 羅盤が戦闘場所から離れてたら消す
-		io_chat.setNextColor(4) -- ピンク
-		local pd = math.floor(mobpetdist * 100 + 0.5) / 100;
-		io_chat.print("ラバンの距離:"..pd)
-		geo_release()
+	inde_setup(jobRank)
+	local pet = windower.ffxi.get_mob_by_target("pet")
+	if pet == nil then  -- 羅盤が無い場合
+	    geo_setup(jobRank)
+	else -- 羅盤がある場合
+	    local mob = windower.ffxi.get_mob_by_target("t")
+	    if mob ~= nil then
+		-- 状況に応じて羅盤解除
+		geo_release_with_contexte(player, pet, mob)
 	    end
 	end
 	if player.vitals.mp >= 1000 then  -- MP に余裕があれば
