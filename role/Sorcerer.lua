12bb8634d61ebb0754a9a7d2662bd7a684d0129b
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-07 01:54:56 +0900

    MB のタイミング調整。

--- role/Sorcerer.lua
+++ role/Sorcerer.lua
@@ -4,7 +4,7 @@ local M = {}
 
 local io_chat = require'io/chat'
 local command = require 'command'
-local asinspect = require 'inspect'
+local acinspect = require 'inspect'
 local task = require 'task'
 
 local MB_magic = "ファイア"
@@ -33,7 +33,10 @@ function invoke_magic(magicRank, onoff)
     assert(type(onoff) == "boolean")
     local param = magickParams[magicRank]
     local level = task.PRIORITY_HIGH
-    local magic = MB_magic..param.rank
+    local magic = MB_magic
+    if magicRank > 1 then
+	magic = magic .. param.rank
+    end
     local c = 'input /ma '..magic..' <t>'
     -- command, delay, duration, period, eachfight
     local t = task.newTask(c, 0, param.dur, param.per, false)
@@ -46,49 +49,67 @@ end
 
 function M.magicBurst(player, magickRank)
     if player.status == 1 then -- 戦闘中
-	local ws_time = asinspect.ws_time -- + 4
+	local sc_time = acinspect.sc_time
+	local sc_attr = acinspect.sc_attr
 	local now = os.time()
 	local mp = player.vitals.mp
-	if within_time(now, ws_time, ws_time + 2)
+	local sc = acinspect.skillchain_table[sc_attr]
+	if sc == nil then
+	    -- print("XXX mb == nil mb_attr:"..mb_attr)
+	    return
+	end
+	MB_magic = sc.magic
+	-- 一旦、FC 少なめでタイミング調整。
+	if within_time(now, sc_time, sc_time + 1)
 	    and magickRank >= 5 and mp >= 306 then
 	    invoke_magic(5, true)
 	else
 	    invoke_magic(5, false)
 	end
-	if within_time(now, ws_time + 2, ws_time + 3)
+	if within_time(now, sc_time, sc_time + 2)
 	    and magickRank >= 4 and mp >= 195 then
 	    invoke_magic(4, true)
 	else
 	    invoke_magic(4, false)
 	end
-	if within_time(now, ws_time + 3, ws_time + 4)
+	if within_time(now, sc_time, sc_time + 3)
 	    and magickRank >= 3 and mp >= 91 then
 	    invoke_magic(3, true)
 	else
 	    invoke_magic(3, false)
 	end
-	if within_time(now, ws_time + 4, ws_time + 5)
+	if within_time(now, sc_time, sc_time + 4)
 	    and magickRank >= 2 and mp >= 37 then
 	    invoke_magic(2, true)
 	else
 	    invoke_magic(2, false)
 	end
+	if within_time(now, sc_time, sc_time + 5)
+	    and magickRank >= 1 and mp >= 9 then
+	    invoke_magic(1, true)
+	else
+	    invoke_magic(1, false)
+	end
+    else  -- 戦闘終了してる場合は、魔法のタスク予約を取り消す。暴発防止
+	invoke_magic(1, false)
+	invoke_magic(2, false)
+	invoke_magic(3, false)
+	invoke_magic(4, false)
+	invoke_magic(5, false)
     end
 end
 
 function M.main_tick(player)
-    if player.status == 1 then -- 戦闘中
-	local magickRank = 2
-	local main_job = player.main_job
-	if main_job == "BLM" then
-	    magickRank = 5
-	elseif main_job == "RDM" then
-	    magickRank = 3
-	elseif main_job == "GEO" then
-	    magickRank = 2
-	end
-	M.magicBurst(player, magickRank)
+    local magickRank = 3
+    local main_job = player.main_job
+    if main_job == "BLM" then
+	magickRank = 5
+    elseif main_job == "RDM" then
+	magickRank = 4
+    elseif main_job == "GEO" or main_job == "DRK" then
+	magickRank = 3
     end
+    M.magicBurst(player, magickRank)
 end
 
 local magicTable = {
