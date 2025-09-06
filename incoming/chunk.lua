d2d6e5da1bf2ca860c3439a15fd623c3a7319c88
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-07 01:36:36 +0900

    デバッグ用のコードをコメントアウトで残す。 incoming chunk handler の id 表示。

--- incoming/chunk.lua
+++ incoming/chunk.lua
@@ -243,9 +243,14 @@ end
 function M.incoming_handler(id, data, modified, injected, blocked)
     local handler = packet_handler[id]
     --print("incoming_handler:",id)
+    local done = false
     if handler ~= nil then
 	local packet = packets.parse('incoming', data)
 	handler(packet)
+	done = true
+    end
+    if not done then  -- デバッグ用
+	-- io_chat.print("incoming_handler id:".. string.format("0x%03X", id))
     end
 end
 
