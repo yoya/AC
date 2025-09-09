7f183ab8db46ffdcf6a94611d75c079fc07e9f6b
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-09 23:10:41 +0900

    ベヒーモスの縄張りでウォンテッドのサポート - CL135 で飛んだら ??? まで移動 - ??? が出現したら自動でターゲット

--- zone/127_BehemotDom.lua
+++ zone/127_BehemotDom.lua
@@ -2,6 +2,8 @@
 
 local M = { id = 127 }
 
+local io_net = require 'io/net'
+
 M.routes = {
     -- CL135 ワープ
     frag = { -- 雷の祈り (古代石碑巡礼)
@@ -11,7 +13,33 @@ M.routes = {
 	{x=-108,y=-20.5}, {x=-102,y=-25}, {x=-98,y=-47.5},
 	{x=-80.5,y=-79}, {a="dismount"}, {x=-75.6,y=-88,z=-4},
     },
+    wanted = { -- ベヒモス
+	{x=-183.1,y=57.9,z=-19.9}, {x=-197.5,y=59.5},
+	{x=-219,y=67,d=1}, {x=-262.5,y=73.3,z=-19.4,d=2},
+    },
 }
+
+M.essentialPoints = {
+    unity_warp = {x=-183.1,y=57.9,z=-19.9},
+    wanted = {x=-269.2,y=74.2,z=-19.8},
+}
+
+M.automatic_routes = {
+    unity_warp = { route="wanted" },
+}
+
+function M.tick(player)
+    local mobArr = windower.ffxi.get_mob_array()
+    -- ウォンテッド???の近くにいる時、??? が現れたらターゲットする
+    if M.parent.isNear(127, "wanted", 20) then
+	for i, m in pairs(mobArr) do
+	    if m.name == "???" and m.status == 0 then
+		io_net.targetByMobId(m.id)
+	    end
+	end
+    end
+end
+
 
 return M
 
