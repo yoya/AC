00e8266c3a022bac34b1fa8883a737c0de5954dd
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-09 23:12:16 +0900

    ホルレーでのトローブ用サポート - ホルレーに入ったらサークルに近づく - BC に入ったら自動で箱の近くまで移動 (たまにフライングするので要調査)

--- zone/139_HorlaisPk.lua
+++ zone/139_HorlaisPk.lua
@@ -1,9 +1,17 @@
 -- ホルレーの岩峰
 
+local contents = require 'contents'
+
 local M = { id = 139 }
 
 M.routes = {
-    trove = {  -- AMAN トローブ
+    -- 入り口
+    circle = {
+	{x=-536,y=-211.6,z=160.5}, {x=-518,y=-210,d=1},
+	{x=-508.4,y=-211.6,z=158.6,d=1},
+    },
+    -- AMAN トローブ
+    trove = {
 	{x=-316.3,y=-102.5}, {x=-346.8,y=-127},
 	{x=-366,y=-121.7}, {x=-381,y=-98},
 	{x=-392.9, y=-71.3},
@@ -17,6 +25,27 @@ M.routes = {
 	{x=163.6,y=257.2}, {x=133.2,y=232.6},
 	{x=110.6,y=242.9}, {x=92.6,y=276.5}, {x=87.1,y=288.4}
     },
+}
+
+function M.warp_in()
+    if M.parent.isNear(139, "entrance", 10) then
+	contents.type = contents.Trove
+    end
+end
+
+M.essentialPoints = {
+    entrance = {x=-536,y=-211.6,z=160.5},  -- ホルレーに入ったところ
+    -- トローブ
+    trove_in = {x=-316.3, y=-102.6,z=113.1,w=15},
+    trove2_in = {x=-75.3,y=77.4,z=-7.1},
+    trove3_in = {x=163.6,y=257.2},
+}
+
+M.automatic_routes = {
+    entrance = { route="circle" },
+    trove_in = { route="trove" },
+    trove2_in = { route="trove2" },
+    trove3_in = { route="trove3" },
 }
 
 return M
