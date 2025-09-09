21145d45e7ba12cfff13b32dccd77745c744768e
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-09 23:09:40 +0900

    typo N. => M.

--- control.lua
+++ control.lua
@@ -5,6 +5,13 @@ local M = {}
 
 M.auto = false
 
+M.ENEMY_SPACE_NEAR = 1
+M.ENEMY_SPACE_MANUAL = 2 -- 動かない。手動操作
+M.ENEMY_SPACE_MAGIC = 3 -- 魔法使い用
+M.ENEMY_SPACE_ROLE = 4 -- 遠隔は遠隔の、魔法使いは魔法ギリギリ(TODO)
+
+M.enemy_space = M.ENEMY_SPACE_NEAR
+
 M.FINISH_ANY = 0
 M.FINISH_ATTACK = 1  -- エミネンス目標
 M.FINISH_RANGE = 2  -- 予定なし
