55e77d5a372c594b2f7074fb9e6bad1c8b26eb87
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-12 02:17:07 +0900

    装束処理のコメント追加。

--- ac/equip.lua
+++ ac/equip.lua
@@ -2,6 +2,7 @@
 
 local M = {}
 
+-- 装束を装着する部位 (slot)
 local equip_slots = {
     main = 0, sub = 1, range = 2, ammo = 3,
     head = 4, body = 5, hands = 6, legs = 7, feet = 8,
@@ -9,6 +10,7 @@ local equip_slots = {
     left_ring = 13, right_ring = 14, back = 15
 }
 
+-- 装束の装着を指定できる bag の種類
 local equip_bags = {
     0, -- inventory
     8, -- wardrobe
@@ -19,15 +21,17 @@ local equip_bags = {
 
 local equip_set = {}
 
+-- 装備中の装束を記録する
 function M.equip_save()
     local items = windower.ffxi.get_items()
     for name, slot  in pairs(equip_slots) do
-	local id = items.equipment[name]
-	local bag = items.equipment[name.."_bag"]
+	local id = items.equipment[name]  -- bag 内 id
+	local bag = items.equipment[name.."_bag"]  -- どの bag か
 	equip_set[name] = { inv_id=id, slot=slot, bag=bag }
     end
 end
 
+-- 記録してある装束を装着し直す
 function M.equip_restore()
     for name, e in pairs(equip_set) do
 	windower.ffxi.set_equip(e.inv_id, e.slot, e.bag)
