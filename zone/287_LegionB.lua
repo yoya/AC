803722df375ea785aef0403cffb5a61d6a508011
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-18 11:29:09 +0900

    アンバスが混んでくると使われる２つ目のインスタンス > 287 LegionB

---
+++ zone/287_LegionB.lua
@@ -0,0 +1,31 @@
+-- アブダルスの模型-レギオンB
+
+local M = { id = 287 }
+
+local contents = require('contents')
+
+function M.zone_in()
+    print("Ambus in")
+    contents.type = contents.Ambus
+end
+
+function M.zone_out()
+    print("Ambus out")
+    contents.type = contents.Idle
+end
+
+M.routes = {
+}
+
+M.essentialPoints = {
+    entrance = {x=137,y=-137,z=12.5},
+}
+
+M.automatic_trust = {
+    "ヴァレンラール", "モンブロー", "セルテウス",
+}
+
+M.automatic_routes = {
+}
+
+return M
