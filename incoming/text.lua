e515cca7e58cec578e0e711d4604a223e03b6845
Yoshihiro Yamazaki <yoya@awm.jp>
2025-09-20 22:59:58 +0900

    lisnter 改善 - ipairs だと歯抜けになった時ループを停止するので pairs に変更 - ac show listener で表示する色の調整

--- incoming/text.lua
+++ incoming/text.lua
@@ -8,7 +8,7 @@ local listener_table_last_idx = 0
 
 function M.incoming_handler(data, modified, original_mode, modified_mode, blocked)
     local text = windower.from_shift_jis(data)
-    for i, listener in ipairs(listener_table) do
+    for i, listener in pairs(listener_table) do
 	if listener ~= nil and string.contains(text, listener.keyword) then
 	    listener.callback(text)
 	end
@@ -20,9 +20,9 @@ function M.addListener(keyword, callback)
     assert(type(callback) == "function")
     local caller_info = debug.getinfo(2)
     local idx = listener_table_last_idx + 1
+    listener_table_last_idx = idx
     listener_table[idx] = { keyword=keyword, callback=callback,
 			    caller_info=caller_info }
-    listener_table_last_idx = idx
     return idx
 end
 
@@ -32,10 +32,13 @@ end
 
 function M.showListener()
     local io_chat = require 'io/chat'
-    for i, listener in ipairs(listener_table) do
+    io_chat.setNextColor(6)
+    io_chat.printf("=== incoming/text.showListener: %d", listener_table_last_idx)
+    for i, listener in pairs(listener_table) do
 	local caller_info = listener.caller_info
 	local caller_name = caller_info.name and caller_info.name or "(nil)"
 	local source_path = caller_info.source:sub(windower.addon_path:len()+1)
+	io_chat.setNextColor(7)
 	io_chat.printf("[%d] keyword=%s caller_func=%s source=%s", i, listener.keyword, caller_name, source_path)
     end
 end
