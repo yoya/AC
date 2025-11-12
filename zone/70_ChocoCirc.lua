2862dc5584ee0ce8752e021fc510cc9edc280bb2
Yoshihiro Yamazaki <yoya@awm.jp>
2025-11-12 13:50:06 +0900

    移動の選択肢でもたつくのを改善。

--- zone/70_ChocoCirc.lua
+++ zone/70_ChocoCirc.lua
@@ -7,19 +7,23 @@ M.routes = {
 	{x=-320,y=-475,z=-0.3}, {x=-326,y=-464},
 	{x=-327.1,y=-456.2}, {x=-329,y=-454},
 	{x=-330,y=-448},
-	{x=-330.3,y=-413}, {a="f8touch"}, {a="wait"},
-	{a="wait"},{a="up"}, {a="wait"}, {a="enter"}
+	{x=-330.3,y=-413},
+	{a="f8touch"}, {a="wait"}, {a="up"}, {a="enter"}
+	--{x=-330.3,y=-413}, {a="f8touch"}, {a="wait"},
+	--{a="wait"},{a="up"}, {a="wait"}, {a="enter"}
     },
     -- アトルガンから入った場所
     aht2warp = {  -- ワープで飛ぶ
 	{x=-149.9,y=-386.4,z=0}, {x=-160,y=-378},
 	{x=-162.5,y=-372,z=0}, {}, {w=3},
-	{a="up"}, {a="wait"}, {a="enter"}
+	{a="up"}, {a="enter"}
+	--{a="up"}, {a="wait"}, {a="enter"}
     },
     aht2warp2ent = {
 	{x=-280,y=-463,z=-4}, {x=-276,y=-466}, {x=-253.1,y=-469.2,z=-5},
-	{a="f8touch"}, {a="wait"},
-	{a="wait"}, {a="up"}, {a="wait"}, {a="enter"}
+	{a="f8touch"}, {a="wait"}, {a="up"}, {a="enter"}
+	--{a="f8touch"}, {a="wait"},
+	--{a="wait"}, {a="up"}, {a="wait"}, {a="enter"}
     },
 }
 
