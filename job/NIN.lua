8e50cbad4c4215b10a6ee207a141ffeb8e5bb193
Yoshihiro Yamazaki <yoya@awm.jp>
2025-11-16 01:47:44 +0900

    忍者に陰忍を追加。あと、Melee に追加

--- job/NIN.lua
+++ job/NIN.lua
@@ -2,8 +2,18 @@
 
 local M = {}
 
-M.mainJobProbTable = { }
+local role_Melee = require 'role/Melee'
+
+M.mainJobProbTable = {
+    { 100, 60*10, 'input /ja 陰忍 <me>', 2 },
+}
 
 M.subJobProbTable = { }
+
+function M.main_tick(player)
+    if role_Melee.main_tick ~= nil then
+	role_Melee.main_tick(player)
+    end
+end
 
 return M
