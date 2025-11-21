526244e39c922ce0030b100f0f867a306f78e271
Yoshihiro Yamazaki <yoya@awm.jp>
2025-11-22 01:32:01 +0900

    必要そうなプロパティを追加 finish は finish_blow に変更。(日本語の、とどめ。の意味)

--- control.lua
+++ control.lua
@@ -1,11 +1,15 @@
--- 全共通のグローバルな制御フラグ/データ
--- config/settings とは分けたい
-
 local M = {
     auto = false,
+    attack = false,
+    puller = false,  -- 基本はリーダーのみ true
+    calm = false,
     debug = false,
+    equip_lock = true,  -- 装備の固定。脱衣攻撃対策
+    -- enemy_space
+    -- finish_blow
 }
 
+-- enemy_space
 M.ENEMY_SPACE_NEAR = 1
 M.ENEMY_SPACE_MANUAL = 2 -- 動かない。手動操作
 M.ENEMY_SPACE_MAGIC = 3 -- 魔法使い用
@@ -13,13 +17,17 @@ M.ENEMY_SPACE_ROLE = 4 -- 遠隔は遠隔の、魔法使いは魔法ギリギリ
 
 M.enemy_space = M.ENEMY_SPACE_NEAR
 
-M.FINISH_ANY = 0
-M.FINISH_ATTACK = 1  -- エミネンス目標
-M.FINISH_RANGE = 2  -- 予定なし
-M.FINISH_MAGIC = 3  -- エミネンス目標、ソーティ課題
-M.FINISH_WS = 4
-M.FINISH_WS_ELEMENT = 5  -- アビセア黄色発光
+-- finish_blow どとめの差し方
+M.FINISH_BLOW_ANY = 0
+M.FINISH_BLOW_ATTACK = 1 -- 通常攻撃でとどめ(エミネンス目標)
+M.FINISH_BLOW_RANGE = 2  -- 遠隔でとどめ(予定なし)
+M.FINISH_BLOW_MAGIC = 3  -- 魔法でとどめ (エミネンス目標、ソーティ課題)
+M.FINISH_BLOW_WS = 4     -- WS でとどめ
+M.FINISH_BLOW_WS_ELEMENT = 5  -- アビセア黄色発光
 
-M.finish = M.FINISH_ANY
+M.finish_blow = M.FINISH_BLOW_ANY
+
+-- 全共通のグローバルな制御フラグ/データ
+-- config/settings とは分けたい
 
 return M
