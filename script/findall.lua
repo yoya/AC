0d87f716110f3c3158f855001c906a52c19fbed6
Yoshihiro Yamazaki <yoya@awm.jp>
2025-11-24 01:47:27 +0900

    - キャラ名を substring で指定。省略時は全キャラクターで処理 - 日本語名を短縮した。正式名だと長過ぎる。

--- script/findall.lua
+++ script/findall.lua
@@ -1,62 +1,91 @@
 #! /usr/bin/lua
 
+local lfs = require('lfs')
+
 package.path = package.path .. ";../?.lua"
 local res_items = require 'res/items'
 local utils = require 'utils'
 local io_console = require 'io/console'
-
-if #arg < 2 then
-    print("Usage: <chara name> <item name substring>")
-    print("ex) lua script/findall.lua Upaupa ヘルクリア")
-    return 1
-end
-
-local chara_name = arg[1]
-local keyword = arg[2]
-
--- print("chara_name:"..chara_name)
-local chara_file = 'findAll/data/'..chara_name
-local chara_data = require(chara_file)
 
 local bag_name_ja_list = {
     { name='inventory', ja='マイバッグ'},
     { name='safe', ja='モグ金庫'},
     { name='safe2', ja='モグ金庫2'},
     { name='storage', ja= '収納家具'},
-    { name='locker', ja='モグロッカー'},
-    { name='satchel', ja='モグサッチェル'},
-    { name='sack', ja='モグサック'},
-    { name='case', ja='モグケース'},
+    --{ name='locker', ja='モグロッカー'},
+    --{ name='satchel', ja='モグサッチェル'},
+    --{ name='sack', ja='モグサック'},
+    --{ name='case', ja='モグケース'},
+    { name='locker', ja='ロッカー'},
+    { name='satchel', ja='サッチェル'},
+    { name='sack', ja='サック'},
+    { name='case', ja='ケース'},
 }
-
-table.insert(bag_name_ja_list, {name='wardrobe', ja='モグワードローブ'})
-
+--table.insert(bag_name_ja_list, {name='wardrobe', ja='モグワードローブ'})
+table.insert(bag_name_ja_list, {name='wardrobe', ja='ローブ'})
 for i = 2, 8 do
     table.insert(bag_name_ja_list, { name='wardrobe'..i,
-				     ja='モグワードローブ'..i})
+				     ja='ローブ'..i})
+				     -- ja='モグワードローブ'..i})
 end
-
 table.insert(bag_name_ja_list, { name='recycle', ja='リサイクル' })
-
 for i = 1, 33 do
     local ii = string.format('%02d', i)
     table.insert(bag_name_ja_list, { name='slip '..ii,
-				     ja='モグの預り帳【'..ii..'】' })
+				     ja='預り帳【'..ii..'】' })
+				     -- ja='モグの預り帳【'..ii..'】' })
 end
-
 table.insert(bag_name_ja_list, { name='key items', ja='だいじなもの' })
 table.insert(bag_name_ja_list, { name='temporary', ja='テンポラリー' })
 
-for _, bag in ipairs(bag_name_ja_list) do
-    local bag_name = bag.name
-    local bag_name_ja = bag.ja
-    local items = chara_data[bag_name]
-    for item_id, count in pairs(items) do
-	local id = tonumber(item_id)
-	local item = res_items[id]
-	if item ~= nil then
-	    if string.find(item.ja, keyword) ~= nil then
-		print(bag_name_ja..": "..item.ja)
+if #arg < 1 then
+    print("Usage: <item name substring> [<chara name>]")
+    print("ex) lua script/findall.lua ヘルクリア")
+    print("ex) lua script/findall.lua ヘルクリア Upaupa ")
+    return 1
+end
+
+local keyword = arg[1]
+local chara_name = arg[2]
+
+-- print("keyword:"..keyword.." chara_name:"..tostring(chara_name))
+
+local chara_name_list = {}
+
+for entry in lfs.dir('findAll/data/') do
+    local m = entry: match "(%w+).lua$"
+    if m then
+	if chara_name == nil or string.find(m, chara_name) ~= nil then
+	    table.insert(chara_name_list, m)
+	end
+    end
+end
+
+function strspacepad(s, n)
+    local l = string.len(s)
+    while l < n do
+	s = s..' '
+	l = l + 1
+    end
+    return s
+end
+
+for _, name in ipairs(chara_name_list) do
+    local chara_file = 'findAll/data/'..name
+    local chara_data = require(chara_file)
+    for _, bag in ipairs(bag_name_ja_list) do
+	local bag_name = bag.name
+	local bag_name_ja = bag.ja
+	local items = chara_data[bag_name]
+	for item_id, count in pairs(items) do
+	    local id = tonumber(item_id)
+	    local item = res_items[id]
+	    if item ~= nil then
+		if string.find(item.ja, keyword) ~= nil then
+		    io_console.printf("%s %s %s",
+				      strspacepad(name, 7),
+				      strspacepad(bag_name_ja, 12), item.ja)
+		end
 	    end
 	end
     end
