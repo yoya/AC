d656ee23001f9d1b92b1de7a2f21a64f6f70c0cf
Yoshihiro Yamazaki <yoya@awm.jp>
2025-12-03 01:07:46 +0900

    予定している制御フラグの整理

--- control.lua
+++ control.lua
@@ -1,28 +1,30 @@
 local M = {
     auto = false,
-    attack = false,
-    puller = false,  -- 基本はリーダーのみ true
-    calm = false,
-    debug = false,
+    attack = false,  -- 敵と戦う
+    calm = false,    -- 戦闘中動かない
+    debug = false,   -- デバッグメッセージ
+    do_my_best = false, -- 本気を出す (1H アビが使う)
     equip_lock = true,  -- 装備の固定。脱衣攻撃対策
+    puller = false,  -- 敵にちょっかい出す。基本はリーダーのみ true
     -- enemy_space
     -- finish_blow
 }
 
 -- enemy_space
-M.ENEMY_SPACE_NEAR = 1
-M.ENEMY_SPACE_MANUAL = 2 -- 動かない。手動操作
-M.ENEMY_SPACE_MAGIC = 3 -- 魔法使い用
-M.ENEMY_SPACE_ROLE = 4 -- 遠隔は遠隔の、魔法使いは魔法ギリギリ(TODO)
+M.ENEMY_SPACE_NEAR   = 1  -- 敵に近づく
+M.ENEMY_SPACE_MANUAL = 2  -- 手動操作。勝手に動かない
+M.ENEMY_SPACE_MAGIC  = 3  -- 魔法使い用
+M.ENEMY_SPACE_RANGE  = 4  -- 遠隔攻撃用
+M.ENEMY_SPACE_ROLE   = 5  -- 遠隔は遠隔の、魔法使いは魔法ギリギリ(TODO)a
 
 M.enemy_space = M.ENEMY_SPACE_NEAR
 
 -- finish_blow どとめの差し方
-M.FINISH_BLOW_ANY = 0
-M.FINISH_BLOW_ATTACK = 1 -- 通常攻撃でとどめ(エミネンス目標)
-M.FINISH_BLOW_RANGE = 2  -- 遠隔でとどめ(予定なし)
-M.FINISH_BLOW_MAGIC = 3  -- 魔法でとどめ (エミネンス目標、ソーティ課題)
-M.FINISH_BLOW_WS = 4     -- WS でとどめ
+M.FINISH_BLOW_ANY    = 0
+M.FINISH_BLOW_ATTACK = 1  -- 通常攻撃でとどめ(エミネンス目標)
+M.FINISH_BLOW_RANGE  = 2  -- 遠隔でとどめ(予定なし)
+M.FINISH_BLOW_MAGIC  = 3  -- 魔法でとどめ (エミネンス目標、ソーティ課題)
+M.FINISH_BLOW_WS     = 4  -- WS でとどめ
 M.FINISH_BLOW_WS_ELEMENT = 5  -- アビセア黄色発光
 
 M.finish_blow = M.FINISH_BLOW_ANY
