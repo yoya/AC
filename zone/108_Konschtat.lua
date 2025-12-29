c39be74587d27c2fbdad0181a7f371bcbd305630
Yoshihiro Yamazaki <yoya@awm.jp>
2025-12-30 00:06:38 +0900

    テレポデム > 醴泉島の移動。

--- zone/108_Konschtat.lua
+++ zone/108_Konschtat.lua
@@ -4,12 +4,18 @@ local M = { id = 108 }
 
 M.routes = {
     -- D.デムリング
-    gusgen = { -- グスゲン鉱山へ
-	{x=220,y=130,z=19.6}, {x=223,y=141,z=19.1},
+    gusgen = {
+	{x=220,y=130,z=19.6,desc="グスゲン鉱山"},
+	{x=223,y=141,z=19.1},
 	{a="mount"}, {x=289,y=129,d=1}, {x=518,y=167,d=1},
 	{x=565,y=172,d=1}, {x=640,y=160,d=1}, {x=663,y=166,d=1},
 	{x=678,y=184,d=1}, {x=687,y=206}, -- {x=697,y=214},
 	{x=705,y=217}, {}
+    },
+    ring2dim = {
+	{x=220,y=130,z=19.6,desc="Dimensional Portal(醴泉島)"},
+	{x=220,y=137,z=19.1}, {wait=2},
+	{target="Dimensional Portal"}, {a="touch"}
     },
 }
 
@@ -18,7 +24,7 @@ M.essentialPoints = {
 }
 
 M.automatic_routes = {
-    -- hp1 = { route="nana" },
+    ring = { route="ring2dim" }
 }
 
 return M
