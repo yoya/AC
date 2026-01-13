c2aa181e6b0480784281acd6851a0373694d310d
Yoshihiro Yamazaki <yoya@awm.jp>
2026-01-14 02:53:40 +0900

    ipc メッセージの arg 数をふやした。

--- io/ipc.lua
+++ io/ipc.lua
@@ -23,27 +23,30 @@ local listener_table_last_idx = 0
 
 local player_name = nil
 
-function M.send(target, method, arg)
+function M.send(target, method, arg1, arg2, arg3)
     local player = windower.ffxi.get_player()
     if control.debug then
-	print("io/ipc.send", SIGNATURE, target, player.name, method, arg)
+	print("io/ipc.send", SIGNATURE, target, player.name, method, arg1)
     end
     local name = "*"
     -- ログイン切り替え時は前のプレイヤー名でメッセージを送る
-    if player ~= nil then
-	player_name = player.name
+    if player ~= nil and player.name ~= nil then
+	name = player.name
     end
-    name = player_name
-    if arg == nil then
+    if arg1 == nil then
 	command = "%s.%s.%s.%s":format(SIGNATURE, target, name, method)
+    elseif arg2 == nil then
+	command = "%s.%s.%s.%s.%s":format(SIGNATURE, target, name, method, arg1)
+    elseif arg3 == nil then
+	command = "%s.%s.%s.%s.%s.%s":format(SIGNATURE, target, name, method, arg1, arg2)
     else
-	command = "%s.%s.%s.%s.%s":format(SIGNATURE, target, name, method, arg)
+	command = "%s.%s.%s.%s.%s.%s.%s":format(SIGNATURE, target, name, method, arg1, arg2, arg3)
     end
     windower.send_ipc_message(command)
 end
 
-function M.send_all(method, arg)
-    M.send("*", method, arg)
+function M.send_all(method, arg1, arg2, arg3)
+    M.send("*", method, arg1, arg2, arg3)
 end
 
 function M.send_party(method, arg)
@@ -79,13 +82,19 @@ function M.recieve(message)
     local target = words[2]
     local source = words[3]
     local method = words[4]
-    local arg = words[5]
+    local arg1 = words[5]
+    local arg2 = words[6]
+    local arg3 = words[7]
     if control.debug then
-	print(target, source, method, arg)
-	if arg == nil then
-	    io_chat.printf("io/ipc.recieve: target:%s source:%s method:%s arg:nil", target, source, method)
+	print(target, source, method, arg1)
+	if arg1 == nil then
+	    io_chat.printf("io/ipc.recieve: target:%s source:%s method:%s arg1:nil", target, source, method)
+	elseif arg2 == nil then
+	    io_chat.printf("io/ipc.recieve: target:%s source:%s method:%s arg1:%s", target, source, method, arg1)
+	elseif arg3 == nil then
+	    io_chat.printf("io/ipc.recieve: target:%s source:%s method:%s arg1:%s arg2:%s", target, source, method, arg1, arg2)
 	else
-	    io_chat.printf("io/ipc.recieve: target:%s source:%s method:%s arg:%s", target, source, method, arg)
+	    io_chat.printf("io/ipc.recieve: target:%s source:%s method:%s arg1:%s arg2:%s arg3:%s", target, source, method, arg1, arg2, arg3)
 	end
     end
     local player = windower.ffxi.get_player()
@@ -108,13 +117,13 @@ function M.recieve(message)
 	    print("not in Party")
 	end
     elseif method == 'all' then
-	M.recieve_all(arg)
+	M.recieve_all(arg1, arg2, arg3)
     elseif method == 'focus' then
-	if M.AC.focusMyIndex == tonumber(arg) then
+	if M.AC.focusMyIndex == tonumber(arg1) then
 	    windower.take_focus()
 	end
     elseif method == 'party' then
-	M.recieve_party(source, arg)
+	M.recieve_party(source, arg1, arg2, arg3)
     else
 	print("io/ipc.recieve: unknown method:"..method)
     end
@@ -141,21 +150,23 @@ function M.warp_with_ring(arg)
 	item_id = 26178
     end
     io_chat.print(item_name.."10-12秒前")
-    coroutine.sleep(math.random(0,8)/6)
+    coroutine.sleep(math.random(1,10)/5)
     local slot_right_ring = 14
     acitem.useEquipItem(slot_right_ring, item_id, item_name, 10)
 end
     
-function M.recieve_all(arg)
+function M.recieve_all(arg1, arg2, arg3)
     if control.debug then
-	print("io/ipc.recieve_all", arg)
+	print("io/ipc.recieve_all", arg1, arg2, arg3)
     end
-    if arg == "dim" or arg == "holla" or arg == "mea" or arg == "warp" then
-	M.warp_with_ring(arg)
-    elseif arg == "reload" then
+    if arg1 == "dim" or arg1 == "holla" or arg1 == "mea" or arg1 == "warp" then
+	M.warp_with_ring(arg1)
+    elseif arg1 == "reload" then
 	task.setTaskSimple("lua u AC; wait 1; lua l AC", 0, 1)
+    elseif arg1 == 'wstp' then
+	control.setWSTP(arg2)
     else
-	print("io/ipc.recieve_all: unknown arg:"..arg)
+	print("io/ipc.recieve_all: unknown arg1:"..arg1)
     end
 end
 
