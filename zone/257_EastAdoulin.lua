bc791a332f4de918c5d9f738fb8ebc5c2e79dee1
Yoshihiro Yamazaki <yoya@awm.jp>
2026-02-03 02:49:13 +0900

    モグハウスを出た時に移動。 オークションへの移動を追加。太陽の広場でベヤルド交換NPCへ移動。 ヤッセの狩場。イオニスで NPC名を指定して処理

--- zone/257_EastAdoulin.lua
+++ zone/257_EastAdoulin.lua
@@ -7,7 +7,13 @@ local io_chat = require 'io/chat'
 M.routes = {
     -- モグハウス
     hp2m = {
-	{x=-53.7,y=-128.5,z=-0.1}, {x=-54,y=-100,z=0,d=1},
+	{x=-53.7,y=-128.5,z=-0.1}, {x=-54,y=-100,z=0,d=2},
+	{x=-54,y=-91,d=1}, {x=-48,y=-90,d=1}, {x=-50,y=-92,d=1},
+	{a="f8"}, {target="Home Point #2"}, {a="touch"}
+    },
+    auction = {
+	{x=-56,y=-128.5,z=-0.1}, {x=-54,y=-99}, {x=-48,y=-94},
+	{x=-24.5,y=-92.3,z=-0.6}, {target="Auction Counter"}
     },
     -- HP#2(M)
     moghouse = {
@@ -46,22 +52,36 @@ M.routes = {
     },
     -- ヤッセの船着場
     ionis = {
-	{x=-57.8,y=85.2,z=-0.1}, {x=-54.3,y=85,z=-0.1}, {w=1},
-	{a="f8touch"}, {a="enter"}, {a="enter"},{a="up"}, {a="enter"},
-	{w=5}, {x=-57.6,y=85.1}, {a="f8touch"}
+	{x=-57.8,y=85.2,z=-0.1}, {x=-54.3,y=85,z=-0.1,d=1},
+	{target="Quiri-Aliri"},
+	{a="enter"}, {wait=1}, {a="enter"},
+	{a="up"}, {a="enter"},
+	{wait=2}, {x=-57.6,y=85.1,d=1}, -- {a="f8touch"},
+	{target="Waypoint"}, {a="touch"}
+    },
+    -- 太陽の広場 (Coronal Esplanade)
+    bayld = { -- ベヤルド交換
+	{x=27.1,y=-60.8,z=-40.2}, {x=62,y=-74}, {x=66,y=-76}, {x=66,y=-109},
+	{x=63,y=-113,z=-40.3,d=1}, {target="Runje Desaali"}
     }
 }
 
 M.essentialPoints = {
-    from_moghouse = {x=-53.7,y=-128.5,z=-0.1},
-    homepoint_2_M = {x=-50.5,y=-95,z=-0.1},
-    yahse_dock = {x=-57.8,y=85.2,z=-0.1}
+    pck = {x=-101.3,y=-10.7,z=-0.1},
+    -- x=(-59.4,-51.5),y=-128.5
+    from_moghouse = {x=-56,y=-128.5,z=-0.1, dx=5,dy=1},
+    -- HP#2(M) x=(-51.3,-50.5), y=(-95.7-93.9)
+    homepoint_2_M = {x=-50.5,y=-95.5,z=-0.1, d=1.5},
+    yahse_dock = {x=-57.8,y=85.2,z=-0.1},
+    sun_square = {x=27.1,y=-60.8,z=-40.2}, -- 太陽の広場 (Coronal Esplanade)
 }
 
 M.automatic_routes = {
+    pck = { route="pck" },
     from_moghouse = { route="hp2m" },
     homepoint_2_M = { route="moghouse" },
-    yahse_dock = { route="ionis" }
+    yahse_dock = { route="ionis" },
+    sun_square = { route="bayld" }, -- ベヤルド交換
 }
 
 return M
