9fd1178b8a15781ea0d6c71358b110d5a1a485fb
Yoshihiro Yamazaki <yoya@awm.jp>
2026-02-03 02:52:56 +0900

    モリマーのコロナイズ移動で、敵にターゲット合わせて戦闘開始

--- zone/265_Morimar.lua
+++ zone/265_Morimar.lua
@@ -13,14 +13,19 @@ M.routes = {
     },
     -- WP#2
     raive1 = {
-	{x=112.8,y=324.4,z=-0.5}, {x=113,y=316,d=1}, {a="mount"},
+	{x=112.8,y=324.4,z=-0.5}, {x=113,y=316,d=1},
+	{a="faith"}, {a="mount"},
 	{x=123,y=248}, {x=74,y=191}, {x=46,y=151}, {x=17,y=96},
 	{x=14,y=93,z=0}, {a="dismount"},
+	{x=0,y=65,z=-0.1}, {target="Bedrock Crag"},
+	{enemy_filter="Bedrock"}, {auto=true}
     },
     raive2 = {
-	{x=-2,y=70,z=-0.4}, {x=-13,y=74}, {a="mount"},
+	{x=0,y=65,z=-0.1}, -- {x=-2,y=70,z=-0.4},
+	{x=-13,y=74}, {a="mount"},
 	{x=-50,y=105}, {x=-72,y=119}, {a="dismount"},
-	{x=-83,y=127,z=0.2},
+	{x=-83,y=127,z=0.2}, {x=-98,y=139,z=-1},
+	{target="Grimy Boulders"}, {enemy_filter="Grimy"}
     }
 }
 
