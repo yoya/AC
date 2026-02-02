ae8b378b8aac79da1efd226250d72ba87eb7e356
Yoshihiro Yamazaki <yoya@awm.jp>
2026-02-03 02:47:37 +0900

    Shemo へのトレードを自動で開始

--- zone/246_PortJeuno.lua
+++ zone/246_PortJeuno.lua
@@ -6,7 +6,7 @@ M.routes = {
    -- HP (E)
     shemo = { {x=36,y=8.8,z=0},{x=-29,y=3},
 	{x=-54.5,y=3.5}, {x=-57,y=8.5}, {x=-55.5,y=10.5},
-	{a="esc"}, {target="Shemo"}
+	{a="esc"}, {target="Shemo"}, {auto=true}
     },
     -- HP (M)
     oboro = {
