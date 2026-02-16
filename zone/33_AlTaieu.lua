ea4971d89672e7baa8abdddabb22c1f27c97465b
Yoshihiro Yamazaki <yoya@awm.jp>
2026-02-16 13:39:38 +0900

    デムからアルタユにワープしたら自動で Vortex2 に移動して触る

--- zone/33_AlTaieu.lua
+++ zone/33_AlTaieu.lua
@@ -8,14 +8,20 @@ M.routes = {
 	{x=-27.5,y=-613.2,z=0}, {x=-1,y=-566,z=0,d=1},
 	{x=-1,y=-547,z=0}, {}
     },
+    vortex2 = {
+	{x=-531,y=447,z=0}, {x=-560,y=444,z=0},
+	{target="Swirling Vortex"}, {a="touch"}
+    },
 }
 
 M.essentialPoints = {
-    from_umijishi = {x=-27.5,y=-613.2,z=0},  -- h12
+    from_umijishi = {x=-27.5,y=-613.2,z=0},  -- H-12
+    from_dem = {x=-531,y=447,z=0}, -- E-6
 }
 
 M.automatic_routes = {
     from_umijishi = { route="huxzoi" },
+    from_dem = { route="vortex2" },
 }
 
 return M
