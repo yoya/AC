3920b9ca5e7be63116617dbe257542e7a3ede0b6
Yoshihiro Yamazaki <yoya@awm.jp>
2026-02-16 13:37:32 +0900

    エスカルオンのドメインワープで龍の足元へ移動

--- zone/289_EschaRuAun.lua
+++ zone/289_EschaRuAun.lua
@@ -5,17 +5,25 @@ local M = { id = 289 }
 M.routes = {
     dremi = {
 	{x=-0.4,y=-467,z=-34.3}, {x=-8.2,y=-461.3,z=-34},
-	{target="Dremi"}
+	{target="Dremi"}, {a="touch"}
     },
+    raja = {
+	{x=0,y=-238,z=-43.8}, -- {x=-4,y=-212,z=-43.6,d=3},
+	--{x=-2,y=-214,z=-43.6,d=3},
+	{x=-4,y=-214,z=-43.6,d=3},
+	{enemy_filter="Naga"}, {puller=true}
+    }
 }
 
 M.essentialPoints = {
     entrance = {x=-0.4,y=-467,z=-34.3},
+    domain_in = {x=0,y=-238,z=-43.8},
 }
 
 
 M.automatic_routes = {
     entrance = { route="dremi" },
+    domain_in = { route="raja" },  -- Naga Raja
 }
 
 return M
