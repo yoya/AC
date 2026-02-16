7d82021fe1d5a272dea7e68d53336e8e6b4a6564
Yoshihiro Yamazaki <yoya@awm.jp>
2026-02-16 13:40:45 +0900

    アポリオンに入ったら自動で Vortex まで移動して触る

--- zone/38_Apollyon.lua
+++ zone/38_Apollyon.lua
@@ -8,6 +8,11 @@ M.routes = {
 	{x=-645,y=-602,z=0}, {target="Apollyon Operator"},
 	{a="touch"}
     },
+    vortex = {
+	{x=-668.4,y=-665.8,z=0}, {x=-648,y=-634},
+	{x=-633,y=-610}, {x=-627,y=-605},
+	{x=-604,y=-600}, {target="Swirling Vortex"}, {a="touch"}
+    },
 }
 
 M.essentialPoints = {
@@ -15,7 +20,8 @@ M.essentialPoints = {
 }
 
 M.automatic_routes = {
-    entrance = { route="ope" },
+    -- entrance = { route="ope" },
+    entrance = { route="vortex" },
 }
 
 return M
