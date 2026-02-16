9c6f224a3112ab9b2c56e17be79f7d17ef3f92c3
Yoshihiro Yamazaki <yoya@awm.jp>
2026-02-16 23:53:54 +0900

    モンクがピンチの時に、生き残る系のアビを使うようにした。

--- job/MNK.lua
+++ job/MNK.lua
@@ -19,15 +19,38 @@ M.mainJobProbTable = {
 
 M.subJobProbTable = {
     { 100, 120, 'input /ja 集中 <me>', 0 },
+    { 100, 120, 'input /ja 回避 <me>', 0 },
 }
 
+
+local inner_strength_command =  "input /ja インナーストレングス <me>"
+local chakra_command = "input /ja チャクラ <me>"
 function M.main_tick(player)
-    if player.status == 1 and player.vitals.hp < 300 then  -- 緊急回復
-	local c = "input /ja インナーストレングス <me>"
-	task.setTaskSimple(c, 0, 2)
+    if player.status == 1 then
+	if player.vitals.hp < 300 then  -- 緊急回復
+	    local params = { level = task.PRIORITY_TOP, period = 1*60*60 }
+	    task.setTaskEx(inner_strength_command, params)
+	elseif 200 < (player.vitals.max_hp - player.vitals.hp) then
+	    local params = { level = task.PRIORITY_TOP, period = 3*60 }
+	    task.setTaskEx(chakra_command, params)
+	else
+	    task.removeTaskEx(inner_strength_command)
+	    task.removeTaskEx(chakra_command)
+	end
     end
     if role_Melee.main_tick ~= nil then
 	role_Melee.main_tick(player)
+    end
+end
+
+function M.sub_tick(player)
+    if player.status == 1 then
+	if 200 < (player.vitals.max_hp - player.vitals.hp) then
+	    local params = { level = task.PRIORITY_TOP, period = 3*60 }
+	    task.setTaskEx(chakra_command, params)
+	else
+	    task.removeTaskEx(chakra_command)
+	end
     end
 end
 
