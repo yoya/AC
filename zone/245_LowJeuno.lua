b4409d96769f2207be538d21463cb9a4020a1a4d
Yoshihiro Yamazaki <yoya@awm.jp>
2026-03-11 20:55:55 +0900

    ジュノ下層(M)からアルドまでの移動

--- zone/245_LowJeuno.lua
+++ zone/245_LowJeuno.lua
@@ -21,6 +21,22 @@ M.routes = {
 	{x=39.5,y=-17.5,z=-0.1}, {x=42,y=-20}, {x=42,y=-22},
 	{x=35,y=-35}, {a="opendoor"}, {x=33,y=-38},
     },
+    aldo = {
+	{x=19,y=53,z=-1,desc="アルド(天晶堂)"},	{x=18,y=47,d=1},
+	{x=18,y=33,d=1}, {x=16,y=29,d=1}, {x=11,y=27,d=1},
+	{x=5,y=16,d=1}, {x=4,y=8,d=1}, {x=12,y=-1},
+	{a="f8"}, {target="Door:\"Neptune's Spire\""}, {a="touch"},
+	{x=15.5,y=-3}, {x=36.2,y=-15.6},
+	{a="f8"}, {target="Door:\"Neptune's Spire\""}, {a="touch"},
+	{x=40,y=-17.8}, {x=42.5,y=-20.5}, {x=36,y=-34},
+	{a="f8"}, {target="Door:Tenshodo H.Q."}, {a="touch"},
+	{x=33,y=-40}, {x=39,y=-56},
+	{a="f8"}, {target="Door:Tenshodo H.Q."}, {a="touch"},
+	{x=37,y=-59}, {x=20,y=-51},
+	{a="f8"}, {target="Door:Aldo's Room"}, {a="touch"},
+	{x=18,y=-54}, {x=20.3,y=-60,d=1},
+	{a="f8"}, {target="Aldo"}, {a="touch"}
+    },
     -- HP (E)
     gob = { {x=-99.6,y=-183.4}, {x=-93.8,y=-150.4},
 	{x=-68.7,y=-102.6},{x=-51.2,y=-113.2}
