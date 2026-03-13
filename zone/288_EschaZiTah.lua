6139ce54d82407505efc96324d17b1df5f96fd33
Yoshihiro Yamazaki <yoya@awm.jp>
2026-03-13 12:36:15 +0900

    エスカジタのドメイン戦闘位置調整

--- zone/288_EschaZiTah.lua
+++ zone/288_EschaZiTah.lua
@@ -11,20 +11,28 @@ M.routes = {
     dahaka = {
 	{x=-2,y=59.5,z=1}, {x=-8,y=46}, -- {x=2,y=37,z=0,d=4},
 	-- {x=0,y=35,z=0,d=3},
-	{x=2,y=38,z=0,d=2},  -- 右足小指
+	-- {x=2,y=38,z=0,d=2},  -- 右足小指
+	{x=3,y=39,z=0,d=2},  -- 右足小指
 	-- {x=-5,y=38,z=0.1,d=2},  -- 左足小指
 	{enemy_filter="Azi"}, {puller=true}
+    },
+    -- wp#2
+    dra = {
+	{x=-303,y=308,z=0.1,"Dragon"}, {x=-312,y=322}, {x=-318,y=339},
+	{x=-317,y=371}
     },
 }
 
 M.essentialPoints = {
     entrance = {x=-345.4,y=-178.9,z=1.1},
     domain_in = {x=-2,y=59.5,z=1},
+    wp2 = {x=-303,y=308,z=0.1,"Dragon"},
 }
 
 M.automatic_routes = {
     entrance = { route="affi" },
     domain_in = { route="dahaka"},  -- Azi Dahaka
+    wp2 = { route="dra" },
 }
     
 return M
