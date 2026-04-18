28fec585a595a85deb77e5bd26bc21ac3a1d38e2
Yoshihiro Yamazaki <yoya@awm.jp>
2026-04-18 22:53:42 +0900

    北サンドリアHP#2からの移動をコンテンツで切り替える。 教皇の部屋への移動経路を追加

--- zone/231_NSandOria.lua
+++ zone/231_NSandOria.lua
@@ -4,29 +4,49 @@ local M = { id = 231 }
 
 M.routes = {
     -- HP#2
-    castle = {  -- 城
-	{x=10,y=94,z=-0.2}, {x=6,y=99}, {x=5,y=107}, {}
+    castle = {
+	{x=10,y=94,z=-0.2,desc="ドラギーユ城"},
+	{x=6,y=99}, {x=5,y=107}, {}
     },
-    phantom = { -- ファントムクォーツ (phantom gem)
-	{x=10,y=94,z=-0.2}, {x=26.5,y=85.7,z=-0.2},
-	{a="f8touch"}
+    phantom = {
+	{x=10,y=94,z=-0.2,desc="Trisvain(ファントムクォーツ)"},
+	{x=26.5,y=85.7,z=-0.2},	{a="f8touch"}
     },
-    -- HP#3
-    daiseido = { -- 大聖堂
-	{x=69,y=9,z=-0.2}, {x=80,y=60,d=1}, {x=92,y=81,d=1},
+    -- HP#3(M)
+    daiseido = {
+	{x=69,y=9,z=-0.2,desc="大聖堂の入り口"},
+	{x=80,y=60,d=1}, {x=92,y=81,d=1},
 	{x=111.9,y=103.8},
-    }
+    },
+    papal = {
+	{x=69,y=9,z=-0.2,desc="教皇の部屋"},
+	{x=80,y=60,d=1}, {x=92,y=81,d=1},
+	{x=111.9,y=103.8},
+	--- 大聖堂の入り口
+	{x=116,y=107,z=0}, {x=127,y=110,z=0}, {x=138,y=99,z=-6.5},
+	{x=137,y=96,z=-6.5}, {x=134,y=96,z=-6.5},
+	{x=124,y=105,z=-6.5}, {x=124,y=107,z=-6.5},
+	{x=135,y=119,z=-11}, {x=135,y=121,z=-11},
+	{x=132,y=123,z=-11,d=1}, {a="f8"},
+	{target="Door:Papal Chambers"}, {a="touch"}
+    },
 }
 
+M.hp2_points = {x=10,y=94,z=-0.2}
+M.hp3_M_points = {x=69,y=9,z=-0.2}
+
 M.essentialPoints = {
-    hp2 = {x=10,y=94,z=-0.2},
-    hp3_M = {x=69,y=9,z=-0.2},
+    hp2 = M.hp2_points,
+    hp2_mission = M.hp2_points,
+    hp3_M = M.hp3_M_points,
+    hp3_M_mission = M.hp3_M_points,
 }
 
 M.automatic_routes = {
-    -- hp2 = { route="castle" },
+    hp2_mission = { route="castle", contents="mission" },
     hp2 = { route="phantom" },
     hp3_M = { route="daiseido" },
+    hp3_M_mission = { route="papal", contents="mission" },
 }
 
 
