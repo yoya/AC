9d84a277a958ffdfea47e864e5fed7a63153c57c
Yoshihiro Yamazaki <yoya@awm.jp>
2026-04-18 23:00:50 +0900

    状況に応じて剣の舞いと扇の舞いを使い分ける。

--- job/DNC.lua
+++ job/DNC.lua
@@ -3,10 +3,9 @@
 local M = {}
 
 local role_Melee = require 'role/Melee'
+local actask = require 'task'
 
 M.mainJobProbTable = {
-    {200, 300, 'input /ja 剣の舞い <me>', 0 },
-    -- {500, 100, 'input /ja 扇の舞い <me>', 0 },
     {50, 90, 'input /ja ヘイストサンバ <me>', 0 },
     {50, 90, 'input /ja ドレインサンバII <me>', 0 },
     {100, 60, 'input /ja B.フラリッシュ <me>', 0 },
@@ -20,9 +19,26 @@ M.mainJobProbTable = {
 
 M.subJobProbTable = { }
 
+function isDefensive()
+    return M.parent.needSafety()
+end
+
 function M.main_tick(player)
     if role_Melee.main_tick ~= nil then
 	role_Melee.main_tick(player)
+    end
+    local level = actask.PRIORITY_LOW
+    local c_a = 'input /ja 剣の舞い <me>'
+    local c_d = 'input /ja 扇の舞い <me>'
+    -- command, delay, duration, period, eachfight
+    local t_a = actask.newTask(c_a, 2, 2, 60*3+1, false)
+    local t_d = actask.newTask(c_d, 2, 2, 60*3+1, false)
+    if true then
+	actask.setTask(level, t_a)
+	actask.removeTask(level, t_d)
+    else
+	actask.removeTask(level, t_a)
+	actask.setTask(level, t_a)
     end
 end
 
