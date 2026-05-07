2822628ef3e25eb52c26b5b560cf057cb368a1db
Yoshihiro Yamazaki <yoya@awm.jp>
2026-05-07 20:28:03 +0900

    竜騎士のアビリティ使用。

--- job/DRG.lua
+++ job/DRG.lua
@@ -2,8 +2,22 @@
 
 local M = {}
 
-M.mainJobProbTable = { }
+M.mainJobProbTable = {
+    { 10, 60*20, 'input /ja コールワイバーン <me>', 2 },
+    { 10, 60*1, 'input /ja ジャンプ <me>', 2 },
+    { 10, 60*1.5, 'input /ja スピリットリンク <me>', 2 },
+    { 10, 60*2, 'input /ja ハイジャンプ <me>', 2 },
+    { 10, 60*3, 'input /ja スーパージャンプ <me>', 2 },
+    { 10, 60*3, 'input /ja スピリットボンド <me>', 2 },
+    { 10, 60*5, 'input /ja ディープブリージング <me>', 2 },
+    -- { 10, 60*3, 'input /ja アンゴン <me>', 2 },
+    { 10, 60, 'input /ja スピリットジャンプ <me>', 2 },
+    { 10, 60*2, 'input /ja ソウルジャンプ <me>', 2 },
+}
 
-M.subJobProbTable = { }
+M.subJobProbTable = {
+    { 10, 60*1, 'input /ja ジャンプ <me>', 2 },
+    { 10, 60*2, 'input /ja ハイジャンプ <me>', 2 },
+}
 
 return M
