e576b9e6739c0cbfcc4cedff53d763771f120d1c
Yoshihiro Yamazaki <yoya@awm.jp>
2026-05-31 11:09:30 +0900

    ガーデンランクが上がる資格がある時に教えてくれる。 お世話はまだ。

--- contents/garden.lua
+++ contents/garden.lua
@@ -28,6 +28,7 @@ end
 
 local listener_idx = 0
 
+-- 同じメッセージで複数 incoming handler が動く事があるので、その対処
 local furrow_check = false  -- 畑
 local pond_check = false  -- 池
 local rearing_check = false  -- モンスター飼育
@@ -47,13 +48,43 @@ M.coast_rank    = -1  -- 海
 M.rearing_rank  = -1  -- お世話
 
 M.rankup_count_table = {
-    furrow   = { 0,  3, 10, 46, 127, 256, 512 },  -- 畑
-    arboreal = { 0 , 9, 20, 74, 195, 300, 750 },  -- 木立
-    mineral  = { 0,  9, 20, 74, 196, 300, 750 },  -- 鉱脈
-    pond     = { 0,  3,  7, 16,  29,  50,  80 },  -- 池
-    coast    = { 0,  3,  7, 16,  29,  50,  80 },  -- 海
-    rearing  = { 0, 15, 20, 40,  60,  80, 100 },  -- お世話
+    furrow   = { 0,  3, 10, 46, 127, 256, 512, 99999 },  -- 畑
+    arboreal = { 0 , 9, 20, 74, 195, 300, 750, 99999 },  -- 木立
+    mineral  = { 0,  9, 20, 74, 196, 300, 750, 99999 },  -- 鉱脈
+    pond     = { 0,  3,  7, 16,  29,  50,  80, 99999 },  -- 池
+    coast    = { 0,  3,  7, 16,  29,  50,  80, 99999 },  -- 海
+    rearing  = { 0, 15, 20, 40,  60,  80, 100, 99999 },  -- お世話
 }
+
+-- M.check_garden_rankup("海", ”coast”)
+function M.check_garden_rankup(name, label)
+    local count = M[label..'_count']
+    local rank = M[label..'_rank']
+    local rankup_table = M.rankup_count_table[label]
+    if count >= 0 and rank >= 0 then
+	if rankup_table[rank+1] <= count then
+	    io_chat.noticef("%sランクアップ(%d => %d), need:%d <= count:%d",
+			    name,  rank, rank + 1, rankup_table[rank+1], count)
+	end
+    end
+end
+
+function M.check_coast_rankup()
+    if M.coast_count >= 0 and M.coast_rank >= 0 then
+	io_chat.printf("海 rank:%d => %d???, count:%d <= %d",
+		       M.coast_rank, M.coast_rank + 1,
+		       M.rankup_count_table.coast[M.coast_rank],
+		       M.coast_count
+	)
+	if M.rankup_count_table.coast[M.coast_rank] <= M.coast_count then
+	    io_chat.printf("海ランクアップ rank:%d => %d, count:%d <= %d",
+			   M.coast_rank, M.coast_rank + 1,
+			   M.rankup_count_table.coast[M.coast_rank],
+			   M.coast_count
+	    )
+	end
+    end
+end
 
 function M.incoming_text_handler(text)
     if string.contains(text, "の記録を") then
@@ -61,10 +92,35 @@ function M.incoming_text_handler(text)
 	pond_check = true
 	rearing_check = true
     end
+    if M.furrow_rank < 0 and string.contains(text, "モーグリの畑") then
+	local s = split_multi(text, {"ランク"})
+	M.furrow_rank = utils.tonumber(s[2])
+	io_chat.info("furrow_rank ", M.furrow_rank)
+	M.check_garden_rankup("畑", "furrow")
+    end
+    if M.arboreal_rank < 0 and string.contains(text, "よく手入れされた木立") then
+	local s = split_multi(text, {"ランク"})
+	M.arboreal_rank = utils.tonumber(s[2])
+	io_chat.info("arboreal_rank ", M.arboreal_rank)
+	M.check_garden_rankup("木立", "arboreal")
+    end
     if M.coast_rank < 0 and string.contains(text, "海岸に仕掛けられた網") then
 	local s = split_multi(text, {"ランク"})
 	M.coast_rank = utils.tonumber(s[2])
 	io_chat.info("coast_rank ", M.coast_rank)
+	M.check_garden_rankup("海", "coast")
+    end
+    if M.pond_rank < 0 and string.contains(text, "淡水の池に仕掛けられた網") then
+	local s = split_multi(text, {"ランク"})
+	M.pond_rank = utils.tonumber(s[2])
+	io_chat.info("pond_rank ", M.pond_rank)
+	M.check_garden_rankup("淡水の池", "pond")
+    end
+    if M.mineral_rank < 0 and string.contains(text, "露出した鉱脈") then
+	local s = split_multi(text, {"ランク"})
+	M.mineral_rank = utils.tonumber(s[2])
+	io_chat.info("mineral_rank ", M.mineral_rank)
+	M.check_garden_rankup("鉱脈", "mineral")
     end
     -- 
     if furrow_check and	string.contains(text, "畑での収穫回数：") then
@@ -86,6 +142,8 @@ function M.incoming_text_handler(text)
 	io_chat.print("pond_count", M.pond_count,
 		      "coast_count", M.coast_count)
 	pond_check = false
+	M.check_garden_rankup("淡水の池", "pond")
+	M.check_garden_rankup("海", "coast")
     end
     if rearing_check and text:contains( "モンスター飼育完了総数：") then
 	local s = split_multi(text, {"お世話した回数：", "モンスター飼育完了総数："})
