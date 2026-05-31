357c9ce888806cee869308bfb9f19f50adc0288d
Yoshihiro Yamazaki <yoya@awm.jp>
2026-05-31 16:09:39 +0900

    setKeys のリストに数値がある場合は wait 扱いする。

--- keyboard.lua
+++ keyboard.lua
@@ -8,9 +8,15 @@ function M.pushKeys(keys)
     local command = ""
     local wait = 0
     for i, k in ipairs(keys) do
-        command = command.."setkey "..k.." down; wait 0.1; setkey "..k.." up; wait 0.1; "
-        wait = wait + 0.1 + 0.1
+	if type(k) == "number" then
+	    command = command.."wait "..k.."; "
+	else
+	    command = command.."setkey "..k.." down; wait 0.1; setkey "..k.." up; wait 0.1; "
+	    wait = wait + 0.1 + 0.1
+	end
     end
+    -- local io_chat = require 'io/chat'
+    -- io_chat.print(command, wait)
     windower.send_command(command)
     coroutine.sleep(wait)
 end
