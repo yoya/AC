b4adb20c3cd24e1233c7824e043c4f4759621aad
Yoshihiro Yamazaki <yoya@awm.jp>
2026-06-12 22:43:05 +0900

    テリガンへのユニティワープからウォンテッドの場所へ自動移動

--- zone/113_Teriggan.lua
+++ zone/113_Teriggan.lua
@@ -12,6 +12,20 @@ M.routes = {
 	{x=-100.5,y=424.5}, {x=-102,y=443}, {a="dismount"},
 	{x=-108,y=451.5,z=-8.1},
     },
+    -- Unityワープ
+    ved = {
+	{x=68.2,y=148,z=-6.2,desc="Vedrfolnir(Unity1,CL128)"},
+	{x=72,y=148,z=-5.3,d=1}, {a="mount"},
+	{x=79,y=148,z=0.2}, {x=140,y=151,z=0.7,d=1}, {a="dismount"}
+    },
+}
+
+M.essentialPoints = {
+    unity_warp = {x=68.2,y=148,z=-6.2},
+}
+
+M.automatic_routes = {
+    unity_warp = { route="ved" },
 }
 
 return M
