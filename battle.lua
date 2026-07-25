8e75ea1e184c33f7d19acd6be5853289f1d71acb
Yoshihiro Yamazaki <yoya@awm.jp>
2026-07-25 21:43:51 +0900

    バトル終了時に移動用装備。バトルで元に戻すようにした。 自分を向いて戦闘するバグをたまにふむので、その時は戦闘解除。: 敵と離れすぎた時も解除

--- battle.lua
+++ battle.lua
@@ -2,6 +2,8 @@
 
 local M = {}
 
+local command = require 'command'
+local ac_equip = require 'ac/equip'
 -- 戦闘スタイル
 
 M.BATTLE_MELEE = 1 -- 物理攻撃
@@ -19,11 +21,48 @@ M.battleTable = {
 }
 for _, obj in pairs(M.battleTable) do obj.parent = M end
 
+M.orig_equip_right_ring_item_id = nil
+
+function M.start()
+    -- print("battle start")
+    -- 戦闘用装備に着替える
+    -- right_ring = 14
+    local slot_name = "right_ring"
+    local slot = 14
+    local item_id = M.orig_equip_right_ring_item_id
+    if item_id ~= nil then
+	ac_equip.equip_item(slot, item_id)
+    end
+end
+
+function M.finish()
+    -- print("battle finish")
+    -- 次に戦う予定がない(attack off でリンクする敵もいない)場合、
+    -- 移動装備に着替える
+    -- right_ring = 14
+    local slot_name = "right_ring"
+    local orig_item_id = ac_equip.equip_item_by_slot_name(slot_name)
+    if orig_item_id ~= nil then
+	M.orig_equip_right_ring_item_id = orig_item_id
+    end
+    local item_id = 27590  -- シュネデックリング
+    local slot = 14
+    ac_equip.equip_item(slot, item_id)
+end
+
 function M.tick(player, me)
+    -- print("battle.tick")
+    local mob = windower.ffxi.get_mob_by_target("t")
+    if mob == nil then return end
+    if mob.name == player.name or mob.distance > 100 then
+	-- 稀に自分をタゲる事があるので、その時は一旦戦闘終了
+	-- 敵と距離がありすぎる時も何かおかしいので戦闘終了
+	command.send('input /attack off')
+    end
     local battle_object = M.battleTable[M.battle_type]
     if battle_object ~= nil then
         if battle_object.tick ~= nil then
-            battle_object.tick(player, me)
+            battle_object.tick(player, me, mob)
         end
     end
 end
