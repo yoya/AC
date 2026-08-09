afee552fe576be21f1a5d3b4dd5c11e2f2958ee9
Yoshihiro Yamazaki <yoya@awm.jp>
2026-08-09 23:55:21 +0900

    アビセア対応

--- zone/117_Tahrongi.lua
+++ zone/117_Tahrongi.lua
@@ -9,14 +9,20 @@ M.routes = {
 	{x=263,y=340,z=35.2,d=1}, {wait=2},
 	{target="Dimensional Portal"}, {a="touch"}
     },
+    maw = {
+        {x=-28.7,y=-682.3,z=46.2,desc="Cavernous Maw(アビセア扉)"}, {wait=2},
+        {target="Cavernous Maw"},
+        {a="touch"}, {wait=1}, {a="up"}, {a="touch"}
+    },
 }
-
 M.essential_points = {
     ring = {x=270,y=340,z=35.7},
+    from_jueno = {x=-28.7,y=-682.3,z=46.2},
 }
 
 M.automatic_routes = {
-    ring = { route="ring2dim" }
+    ring = { route="ring2dim" },
+    from_jueno = { route="maw" },
 }
 
 return M
