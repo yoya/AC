03e719bd54858bbc347367ece55852a055e5d467
Yoshihiro Yamazaki <yoya@awm.jp>
2026-08-10 11:04:25 +0900

    zone 256(西アドゥリン)で自動で Redeem contents にならず Redeem は明示的に指定して動かす事にしたので、enable unity point redeem フラグをデフォルト true にした。

--- contents/redeem.lua
+++ contents/redeem.lua
@@ -21,7 +21,7 @@ M.unity_point_redeem_enable = true
 function M.contents_in(player)
     print("contents/redeem.contents_in")
     M.eminence_point_redeem_enable = true
-    M.unity_point_redeem_enable = false -- エミネンス交換するまで off
+    M.unity_point_redeem_enable = true
 end
 
 function M.incoming_text_handler(text)
@@ -29,14 +29,12 @@ function M.incoming_text_handler(text)
 	return
     end
     local me = windower.ffxi.get_mob_by_target("me")
-    local t = windower.ffxi.get_mob_by_target("t")
     local eminence_point = ac_char.eminence_point()
     local unity_point = ac_char.unity_point()
     -- エミネンス交換後
     if string.contains(text, "sparks to purchase:") then
 	--push_keys({1, "escape", "escape"})
 	push_keys({1, "numpad*"}) -- lock を外す
-	M.unity_point_redeem_enable = true
     elseif string.contains(text, "Sparks Buying Finished.") or
 	string.contains(text, "アイテムを整理した後") then
 	windower.ffxi.run(24 - me.x, -120 - me.y)
@@ -81,24 +79,28 @@ function M.incoming_text_handler(text)
 	if 5000 < exchange_point then
 	    exchange_point = 5000
 	end
+	local unity_point_div_10 = math.floor(ac_char.unity_point()/10)
+	if unity_point_div_10 < exchange_point then
+	    exchange_point = unity_point_div_10
+	end
 	push_keys({1, "right", "down", "enter"}) -- どうする？
 	push_keys({2, "right", "right", "right", "enter"}) -- 次へ進む
 	push_keys({2, "right", "right", "enter"}) -- プライズパウダー
 	io_chat.info("ユニティポイント交換額", exchange_point)
 	coroutine.sleep(3)
 	for c in tostring(exchange_point):gmatch(".") do
-	    io_chat.print(c)
+	    -- io_chat.print(c)
 	    -- push_keys({c}) -- これで数字入力できない。
 	    M.dialog_keyinput(c)
-	    coroutine.sleep(1)
+	    coroutine.sleep(0.5)
 	end
-	M.dialog_keyinput("enter")
+	push_keys({"enter"})
     end
 end
 
 function M.dialog_keyinput(c)
-    command.send("keyboard_sendstring \""..c.."\"")
-    -- command.send("keyboard_type \""..c.."\"")
+    -- command.send("keyboard_sendstring "..c)  -- say で出ちゃった
+    command.send("keyboard_type "..c)
 end
 
 return M
