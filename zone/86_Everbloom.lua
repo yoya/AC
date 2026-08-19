91f26532e125eddb61bda9447f929215e00de09b
Yoshihiro Yamazaki <yoya@awm.jp>
2026-08-20 01:10:31 +0900

    ラテーヌ草原から入ったところ。BF モーグリのありがとうクポ で入った場所の座標を記録しておくだけ。

--- zone/86_Everbloom.lua
+++ zone/86_Everbloom.lua
@@ -3,7 +3,8 @@
 local M = { id = 86 }
 
 M.essential_points = {
-    entrance = {x=240,y=-332,z=20}  -- BF モーグリのありがとうクポ
+    -- ラテーヌ草原から入ったところ。BF モーグリのありがとうクポ
+    entrance = {x=240,y=-332,z=20}
 }
 
 return M
